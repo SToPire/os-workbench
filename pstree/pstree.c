4c895e9829e3f84f1f2d3b0254f1b2d743d34280
tracer-nju <tracer@nju.edu.cn>
2020-02-17 17:52:54 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: c44d3451299a4c10a2529370e6ce93be   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  17:52:54 up  2:52,  1 user,  load average: 1.37, 0.80, 0.57

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -22,10 +22,12 @@ int main(int argc, char *argv[]) {
           sprintf(path, "%s/%d/status", PROC_BACE, pid);
           FILE* file;
           if((file = fopen(path,"r")) != NULL){
-              char BUF[128*8],name[32],unused[32],Tgid[16],Pid[16],PPid[16];
+              int Tgid, Pid, PPid;
+              char BUF[128*8],name[32],unused[32];
               fread(BUF, 1, 128, file);
-              sscanf(BUF, "Name:%s\nUmask:%s\nState:%s\nTgid:%s\nNgid:%s\nPid:%s\nPPid:%s\n", name,unused,unused,Tgid,unused,Pid,PPid);
-              printf("%s:%s %s %s\n", name, Tgid, Pid, PPid);
+              sscanf(BUF, "Name:%s\nUmask:%s\nState:%s\nTgid:%d\nNgid:%s\nPid:%d\nPPid:%d\n", name,unused,unused,&Tgid,unused,&Pid,&PPid);
+              //printf("%s:%d %d %d\n", name, Tgid, Pid, PPid);
+              printf("%s %s", name, unused);
               printf("%s\n\n", BUF);
           }
       }
