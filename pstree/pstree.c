5b2cfdac37d1b9293458ceff0a8982bcde3db620
tracer-nju <tracer@nju.edu.cn>
2020-02-17 21:18:34 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: fc212f0062f74ac298d21d950cc3ca21   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  21:18:34 up 26 min,  1 user,  load average: 1.11, 0.65, 0.47

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -4,32 +4,44 @@
 #include<dirent.h>
 #include<stdlib.h>
 
+struct Node{
+    char name[32];
+    int pid;
+    struct Node* children;
+} head, tail;
 
-int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]);
+int main(int argc, char* argv[])
+{
+    for (int i = 0; i < argc; i++) {
+        assert(argv[i]);
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+    assert(!argv[argc]);
+    strcpy("00000", head.name);
+    strcpy("zzzzz", tail.name);
+    head.pid = 0;
+    tail.pid = 2148473647;
+    print("%d\n", tail.pid);
 
-  DIR* dir = opendir("/proc");
-  struct dirent* dir_entry;
-  while ((dir_entry = readdir(dir)) != NULL) {
-      pid_t pid;
-      if((pid = (pid_t)atoi(dir_entry->d_name)) != 0){
-          char path[32];
-          sprintf(path, "/proc/%d/status", pid);
-          FILE* file;
-          if((file = fopen(path,"r")) != NULL){
-              int Tgid, Pid, PPid;
-              char BUF[128 * 8], name[32];
-              fread(BUF, 1, 128, file);
-              sscanf(BUF, "Name: %[^\n]\nUmask:%*s\nState:%*[^\n]\nTgid:%d\nNgid:%*s\nPid:%d\nPPid:%d\n", name, &Tgid, &Pid, &PPid);
-              printf("%s %d %d %d\n", name, Tgid, Pid, PPid);
-              printf("%s\n\n", BUF);
-          }
-      }
-  }
 
-  return 0;
+    // DIR* dir = opendir("/proc");
+    // struct dirent* dir_entry;
+    // while ((dir_entry = readdir(dir)) != NULL) {
+    //     pid_t pid;
+    //     if ((pid = (pid_t)atoi(dir_entry->d_name)) != 0) {
+    //         char path[32];
+    //         sprintf(path, "/proc/%d/status", pid);
+    //         FILE* file;
+    //         if ((file = fopen(path, "r")) != NULL) {
+    //             int Tgid, Pid, PPid;
+    //             char BUF[128 * 8], name[32];
+    //             fread(BUF, 1, 128, file);
+    //             sscanf(BUF, "Name: %[^\n]\nUmask:%*s\nState:%*[^\n]\nTgid:%d\nNgid:%*s\nPid:%d\nPPid:%d\n", name, &Tgid, &Pid, &PPid);
+    //             printf("%s %d %d %d\n", name, Tgid, Pid, PPid);
+    //             printf("%s\n\n", BUF);
+    //         }
+    //     }
+    // }
+
+    return 0;
 }
