355729421d9b744e82bbe3039516a725bdc8c6e6
tracer-nju <tracer@nju.edu.cn>
2020-02-21 22:21:43 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 281ac3f6a20049cda794661037dfbe9d   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  22:21:43 up 27 min,  1 user,  load average: 0.39, 0.62, 0.54

--- amgame/src/video.c
+++ amgame/src/video.c
@@ -2,7 +2,7 @@
 
 #define SIDE 16
 static int w, h;
-extern int x, y, vx, vy,FPS;
+extern int x, y, vx, vy, FPS;
 static void init()
 {
     _DEV_VIDEO_INFO_t info = {0};
@@ -11,33 +11,39 @@ static void init()
     h = info.height;
 }
 
-static void draw_tile(int x, int y, int w, int h, uint32_t color) {
-  uint32_t pixels[w * h]; // careful! stack is limited!
-  _DEV_VIDEO_FBCTRL_t event = {
-    .x = x, .y = y, .w = w, .h = h, .sync = 1,
-    .pixels = pixels,
-  };
-  for (int i = 0; i < w * h; i++) {
-    pixels[i] = color;
-  }
-  _io_write(_DEV_VIDEO, _DEVREG_VIDEO_FBCTRL, &event, sizeof(event));
+static void draw_tile(int x, int y, int w, int h, uint32_t color)
+{
+    uint32_t pixels[w * h];  // careful! stack is limited!
+    _DEV_VIDEO_FBCTRL_t event = {
+        .x = x,
+        .y = y,
+        .w = w,
+        .h = h,
+        .sync = 1,
+        .pixels = pixels,
+    };
+    for (int i = 0; i < w * h; i++) {
+        pixels[i] = color;
+    }
+    _io_write(_DEV_VIDEO, _DEVREG_VIDEO_FBCTRL, &event, sizeof(event));
 }
 
-void splash() {
-  init();
-  for (int x = 0; x * SIDE <= w; x ++) {
-    for (int y = 0; y * SIDE <= h; y++) {
-      if ((x & 1) ^ (y & 1)) {
-        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
-      }
+void splash()
+{
+    init();
+    for (int x = 0; x * SIDE <= w; x++) {
+        for (int y = 0; y * SIDE <= h; y++) {
+            if ((x & 1) ^ (y & 1)) {
+                draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff);  // white
+            }
+        }
     }
-  }
 }
 
 void screen_update()
 {
     init();
-    draw_tile(w / 2, h / 2, 1, 1, 0xff00ff);
+    draw_tile(x + w / 2, y + h / 2, 1, 1, 0xff00ff);
 }
 
 void game_progress()
