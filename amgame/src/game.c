95a03590603acdcc41de58019e6357fdd693adbd
tracer-nju <tracer@nju.edu.cn>
2020-02-21 21:47:07 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 59ccefd0891e4d3b9a1b47b8e059dd49   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  21:47:07 up  1:11,  1 user,  load average: 0.87, 0.59, 0.60

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -1,18 +1,46 @@
 #include <game.h>
-#include<klib.h>
+#include <klib.h>
+
+#define FPS 30
+
+void kbd_event(int keycode);
+
 // Operating system is a C program!
-int main(const char *args) {
-  _ioe_init();
+int main(const char* args)
+{
+    _ioe_init();
 
-  puts("mainargs = \"");
-  puts(args); // make run mainargs=xxx
-  puts("\"\n");
+    // puts("mainargs = \"");
+    // puts(args); // make run mainargs=xxx
+    // puts("\"\n");
 
-  splash();
-  printf("sad");
-  puts("Press any key to see its key code...\n");
-  while (1) {
-      print_key();
-  }
-  return 0;
+    // splash();
+    // puts("Press any key to see its key code...\n");
+    // while (1) {
+    //     print_key();
+    // }
+
+    int next_frame = 0;
+    int key;
+    while (1) {
+        while (uptime() < next_frame)
+            ;  // 等待一帧的到来
+        while ((key = read_key()) != _KEY_NONE) {
+            kbd_event(key);  // 处理键盘事件
+        }
+        //game_progress();           // 处理一帧游戏逻辑，更新物体的位置等
+        //screen_update();           // 重新绘制屏幕
+        next_frame += 1000 / FPS;  // 计算下一帧的时间
+    }
+
+    return 0;
+}
+
+void kbd_event(int keycode)
+{
+#define KEYNAME(key) \
+    [_KEY_##key] = #key,
+    static const char* key_names[] = {
+        _KEYS(KEYNAME)};
+    printf("You have just pressed key: %s\n", key_names[keycode]);
 }
