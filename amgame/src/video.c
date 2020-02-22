c428d0ab1ca0ba22184a937b1a6d11d908cc3c0f
tracer-nju <tracer@nju.edu.cn>
2020-02-22 16:52:19 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 2c1da0110c6e49b780eb1fbd328fa255   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  16:52:19 up 6 min,  1 user,  load average: 0.71, 0.54, 0.29

--- amgame/src/video.c
+++ amgame/src/video.c
@@ -10,6 +10,8 @@ static void init()
     _io_read(_DEV_VIDEO, _DEVREG_VIDEO_INFO, &info, sizeof(info));
     w = info.width;
     h = info.height;
+
+    printf("%d %d", w, h);
 }
 
 static void draw_tile(int x, int y, int w, int h, uint32_t color)
