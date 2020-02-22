a05e20c8f1b468b2f00e8641e991f4db040ccc1c
tracer-nju <tracer@nju.edu.cn>
2020-02-22 20:45:04 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 2c1da0110c6e49b780eb1fbd328fa255   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  20:45:04 up  3:59,  1 user,  load average: 0.32, 1.05, 1.19

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -63,8 +63,8 @@ void kbd_event(int keycode)
         _KEYS(KEYNAME)};
     if(1){
         if (keycode == _KEY_ESCAPE) _halt(0);
-        if (keycode == _KEY_W || (keycode ^ 0x8000) == _KEY_W) Gear += 1;
-        if (keycode == _KEY_S) Gear -= 1;
+        if (keycode == _KEY_W || (keycode ^ 0x8000) == _KEY_W) if(Gear != 5) Gear += 1;
+        if (keycode == _KEY_S) if(Gear != 0) Gear -= 1;
         printf("You have just pressed key: %s %d\n", key_names[keycode], keycode);
     }
 }
