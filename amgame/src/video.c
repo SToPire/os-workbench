533e7c4fbbf68c8bc87b8e116d2cf50d9ad293da
tracer-nju <tracer@nju.edu.cn>
2020-02-22 22:36:27 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 2c1da0110c6e49b780eb1fbd328fa255   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  22:36:27 up  5:50,  1 user,  load average: 1.54, 1.20, 1.03

--- amgame/src/video.c
+++ amgame/src/video.c
@@ -39,8 +39,8 @@ void draw_bdr(int bdr_w, int bdr_h)
     int beg_x = (w - bdr_w) / 2;
     int beg_y = (h - bdr_h) / 2;
     //draw_tile(beg_x, beg_y, bdr_w, 1, bdr_color);
-    draw_tile(beg_x, beg_y, 1, bdr_h, bdr_color);
-    draw_tile(beg_x + bdr_w, beg_y, 1, bdr_h, bdr_color);
+    draw_tile(beg_x, beg_y, 1, bdr_h+10, bdr_color);
+    draw_tile(beg_x + bdr_w, beg_y, 1, bdr_h+10, bdr_color);
     //draw_tile(beg_x, beg_y + bdr_h, bdr_w, 1, bdr_color);
 }
 
