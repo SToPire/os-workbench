91858acda6648a57ad3e6466c59692d1291ab79c
tracer-nju <tracer@nju.edu.cn>
2020-02-22 21:32:35 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 2c1da0110c6e49b780eb1fbd328fa255   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  21:32:35 up  4:46,  1 user,  load average: 1.33, 1.41, 1.29

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -44,7 +44,7 @@ void global_initial()
 {
     _ioe_init();
     init_screen();
-    srand(time(0));
+    srand(uptime());
     w = screen_width();
     h = screen_height();
     bdr_w = 300, bdr_h = 180;
