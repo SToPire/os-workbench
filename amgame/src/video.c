48f20fd3807ef53f233a16f39e2a6229206487b3
tracer-nju <tracer@nju.edu.cn>
2020-02-22 21:02:24 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 2c1da0110c6e49b780eb1fbd328fa255   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  21:02:25 up  4:16,  1 user,  load average: 1.04, 1.08, 1.08

--- amgame/src/video.c
+++ amgame/src/video.c
@@ -2,6 +2,10 @@
 #include<klib.h>
 #define SIDE 16
 extern int w, h;
+extern struct carPosition {
+    int x;
+    int y;
+} carPositions[5];
 void init_screen()
 {
     _DEV_VIDEO_INFO_t info = {0};
@@ -25,23 +29,6 @@ void draw_tile(int x, int y, int w, int h, uint32_t color)
         pixels[i] = color;
     }
     _io_write(_DEV_VIDEO, _DEVREG_VIDEO_FBCTRL, &event, sizeof(event));
-}
-
-void splash()
-{
-    init_screen();
-    // for (int x = 0; x * SIDE <= w; x++) {
-    //     for (int y = 0; y * SIDE <= h; y++) {
-    //         if ((x & 1) ^ (y & 1)) {
-    //             draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff);  // white
-    //         }
-    //     }
-    // }
-    for (int x = 0; x <= w;x=x+10){
-        for (int y = 0; y < h;y=y+10){
-            draw_tile(x, y, 10, 10, 0xff0000);
-        }
-    }
 }
 
 void draw_bdr(int bdr_w, int bdr_h)
@@ -72,4 +59,8 @@ void draw_car(int car_x,int car_y, uint32_t color)
     draw_tile(car_x, car_y + 15, 2, 2, color);
     draw_tile(car_x + 12, car_y +4, 2, 2, color);
     draw_tile(car_x + 12, car_y + 15, 2, 2, color);
+    if(color==0xff0000){
+        carPositions[0].x = car_x;
+        carPositions[0].y = car_y;
+    }
 }
