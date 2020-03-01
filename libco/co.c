0444ca9ee47c4bee364c29d70a97136e207d483c
tracer-nju <tracer@nju.edu.cn>
2020-03-02 00:27:17 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: dba96ce522d94e0e9f47b3cbc4e6c297   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  00:27:17 up  3:34,  1 user,  load average: 0.84, 0.45, 0.32

--- libco/co.c
+++ libco/co.c
@@ -1,9 +1,13 @@
 #include "co.h"
 #include <stdlib.h>
-#include<setjmp.h>
-#include<inttypes.h>
+#include <setjmp.h>
+#include <inttypes.h>
+#include <string.h>
+#include  <am.h>
+#include  <amdev.h>
+#define STACK_SIZE 64*1024
 
-#define STACK_SIZE 64
+struct co* current;
 
 enum co_status {
     CO_NEW = 1,  // 新创建，还未执行过
@@ -25,12 +29,31 @@ struct co {
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
     struct co* ptr = malloc(sizeof(struct co));
-    return NULL;
+    strcpy(ptr->name, name);
+    ptr->func = func;
+    ptr->arg = arg;
+
+    ptr->status = CO_NEW;
+    ptr->waiter = NULL;
+
+    return ptr;
 }
 
 void co_wait(struct co *co) {
-  
+    int val = setjmp(current->context);
+    if(val==0){
+      stack_switch_call
+    }else{
+        free(co);
+        return;
+    }
 }
 
 void co_yield() {
+    int val = setjmp(current->context);
+    if (val == 0) {
+        // ?
+    } else {
+        return;
+    }
 }
