ce9d278b68302ebfe9240725d3a4c9caca27751d
tracer-nju <tracer@nju.edu.cn>
2020-03-02 02:19:35 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: dba96ce522d94e0e9f47b3cbc4e6c297   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  02:19:35 up  5:26,  1 user,  load average: 0.60, 0.24, 0.16

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -18,8 +18,7 @@ static void work_loop(void *arg) {
     const char *s = (const char*)arg;
     for (int i = 0; i < 100; ++i) {
         printf("112\n");
-
-        printf("%s%d  ", s, get_count());
+        printf("%s%d  ", "sss", get_count());
         add_count();
         co_yield();
     }
