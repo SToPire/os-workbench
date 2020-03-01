497740b2796a9782b7ccc649e10a43ee457c6577
tracer-nju <tracer@nju.edu.cn>
2020-03-02 02:18:31 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: dba96ce522d94e0e9f47b3cbc4e6c297   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  02:18:31 up  5:25,  1 user,  load average: 0.08, 0.10, 0.11

--- libco/co.c
+++ libco/co.c
@@ -60,7 +60,6 @@ struct co* co_start(const char* name, void (*func)(void*), void* arg)
 
 void co_wait(struct co* co)
 {
-    printf("112\n");
     current = co;
     stack_switch_call(co->stack+STACK_SIZE, co->func, (uintptr_t)co->arg);
 }
