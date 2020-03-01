c748256f688523f6f962aa9e1feafd2e95e309b3
tracer-nju <tracer@nju.edu.cn>
2020-03-02 04:58:27 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 54c4162952c94b33af20724cf4d2ebf4   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  04:58:27 up 2 min,  1 user,  load average: 1.39, 0.66, 0.26

--- libco/co.c
+++ libco/co.c
@@ -69,7 +69,6 @@ void co_yield()
     int val = setjmp(current->context);
     if (val == 0) {
         int r = rand() % 2;
-        printf("%d\n", r);
         current = colist[r];
         if (colist[r]->status == CO_NEW)
             stack_switch_call(colist[r]->stack+STACK_SIZE, colist[r]->func, (uintptr_t)colist[r]->arg);
