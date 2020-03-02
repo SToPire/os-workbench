f5b6bbe56915695d5e14dd695a49628fd1f503b5
tracer-nju <tracer@nju.edu.cn>
2020-03-03 00:57:59 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 9f3c7e81b08d4f419dcec01990b2141c   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  00:57:59 up  4:01,  1 user,  load average: 0.29, 0.37, 0.32

--- libco/co.c
+++ libco/co.c
@@ -114,7 +114,7 @@ void co_yield()
         int r = rand() % CO_SIZE;
         while (colist[r]==NULL || colist[r]->status==0 || colist[r]->status == CO_WAITING || colist[r]->status == CO_DEAD)
             r = rand() % CO_SIZE;
-        printf("r=%d\n", r);
+        printf("r=%d colist[r]->status:%d\n", r,colist[r]->status);
         current = colist[r];
         if (current->status == CO_NEW) {
             current->status = CO_RUNNING;
