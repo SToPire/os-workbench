9a9e12135618d8a2b6679e13c89b74d4f133e232
tracer-nju <tracer@nju.edu.cn>
2020-03-02 22:22:47 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 9f3c7e81b08d4f419dcec01990b2141c   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  22:22:47 up  1:26,  1 user,  load average: 0.40, 0.25, 0.20

--- libco/co.c
+++ libco/co.c
@@ -50,9 +50,9 @@ __attribute__((constructor)) void co_init()
 {
     srand(time(0));
     colistcnt = 1;
-    printf("here\n");
 
     colist[0]->status = CO_RUNNING;
+    printf("here\n");
     current = colist[0];
 }
 struct co* co_start(const char* name, void (*func)(void*), void* arg)
