5f67b5085173ddc600fce48d9dc6acd79f66321e
tracer-nju <tracer@nju.edu.cn>
2020-03-23 01:37:00 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 969d643e57364aad8eca9dbf3871f92e   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  01:37:00 up  4:08,  1 user,  load average: 2.69, 1.39, 1.06

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -106,7 +106,7 @@ static void pmm_init()
         //printf("%p %p\n", pages[i].header, pages[i].data);
     }
     freePageHead = &pages[0];
-    printf("%p %p\n", pages, freePageHead);
+    printf("%p %p\n", pages[0].nxt, freePageHead);
     //printf("%d %d %d\n", HDR_SIZE, PAGE_SIZE, sizeof(page_t));
     //printf("%p %p %p %d\n", _heap.end, kmem_cache, ((uintptr_t)kmem_cache & ((2 * PAGE_SIZE - 1) ^ (~PAGE_SIZE))), PAGE_NUM);
 }
