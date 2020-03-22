a54842833f7982f7c4548f8768466e81afea3eec
tracer-nju <tracer@nju.edu.cn>
2020-03-22 18:28:05 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 45351941b31c45708f1c55000c4a490f   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  18:28:05 up  1:00,  1 user,  load average: 0.25, 0.33, 0.29

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,19 +1,38 @@
 #include <common.h>
+#include <lock.h>
 
-static void *kalloc(size_t size) {
-  return NULL;
+#define HDR_SIZE sizeof(spinlock_t) + sizeof(int)
+#define PAGE_SIZE (8 << 10)
+typedef union page {
+    struct {
+        spinlock_t lock;  // 锁，用于串行化分配和并发的 free
+        int obj_cnt;      // 页面中已分配的对象数，减少到 0 时回收页面
+        //list_head list;   // 属于同一个线程的页面的链表
+    };  // 匿名结构体
+    struct {
+        uint8_t header[HDR_SIZE];
+        uint8_t data[PAGE_SIZE - HDR_SIZE];
+    } __attribute__((packed));
+} page_t;
+static void* kalloc(size_t size)
+{
+    return NULL;
 }
 
-static void kfree(void *ptr) {
+static void kfree(void* ptr)
+{
 }
 
-static void pmm_init() {
-  uintptr_t pmsize = ((uintptr_t)_heap.end - (uintptr_t)_heap.start);
-  printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, _heap.start, _heap.end);
+static void pmm_init()
+{
+    uintptr_t pmsize = ((uintptr_t)_heap.end - (uintptr_t)_heap.start);
+    printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, _heap.start, _heap.end);
+
+    printf("%d\n", HDR_SIZE);
 }
 
 MODULE_DEF(pmm) = {
-  .init  = pmm_init,
-  .alloc = kalloc,
-  .free  = kfree,
+    .init = pmm_init,
+    .alloc = kalloc,
+    .free = kfree,
 };
