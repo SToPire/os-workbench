ab9a8c32102221995bf8e70794babd1b405616ef
tracer-nju <tracer@nju.edu.cn>
2020-03-23 16:00:52 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 39cba04b8b3a4fb3a249249752886aad   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-29-generic       Architecture: x86-64  16:00:52 up  2:20,  1 user,  load average: 0.25, 0.22, 0.29

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -143,10 +143,10 @@ static void pmm_init()
     printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, _heap.start, _heap.end);
 
     kmem_cache = (cache_t**)((uintptr_t)_heap.end - 13 * CPU_NUM*sizeof(cache_t));
+    printf("here\n");
     for (int i = 0; i < CPU_NUM; i++)
         for (int j = 0; j < 13; ++j) 
             kmem_cache[i][j].list = NULL;
-    printf("here\n");
     pages = (page_t*)_heap.start;
     const int PAGE_NUM = (((uintptr_t)kmem_cache & ((2 * PAGE_SIZE - 1) ^ (~PAGE_SIZE))) - (uintptr_t)_heap.start) / PAGE_SIZE;
     for (int i = 0; i < PAGE_NUM; ++i) {
