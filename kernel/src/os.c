a941af97bb31275e71cbb8cb86dd2062c42f0247
tracer-nju <tracer@nju.edu.cn>
2020-03-24 23:45:16 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 2cdac1feaa6f4be6afdb08fe593ffa16   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  23:45:16 up 10:25,  1 user,  load average: 2.06, 1.67, 0.91

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -15,7 +15,7 @@ static void os_run()
     // for (int i = 1; i <= 2000; i++) pmm->alloc(4096);
     // for (int i = 1; i <= 2000; i++) pmm->alloc(2048);
     // for (int i = 1; i <= 2000; i++) pmm->alloc(1024);
-    // for (int i = 1; i <= 2000; i++) pmm->alloc(512);
+    for (int i = 1; i <= 2000; i++) pmm->alloc(512);
     for (int i = 1; i <= 8000; i++) pmm->alloc(8);
 
     // if (_cpu() == 1) {
