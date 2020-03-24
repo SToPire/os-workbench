bb2074aa9052b856262acefa6f026979afbf7f2b
tracer-nju <tracer@nju.edu.cn>
2020-03-25 03:04:22 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: e3b7744750484a4ca764db06d4ff95fc   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  03:04:22 up 53 min,  1 user,  load average: 0.27, 0.59, 0.87

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -44,7 +44,7 @@ bool isUnitUsing(page_t* page, bool num)
 {
     printf("num/64=%d num%64=%d page->bitmap[num/64]=%lu 1 << (num % 64)=%d ret=%d\n",
            num / 64, num % 64, page->bitmap[num / 64], (1 << (num % 64)), (page->bitmap[num / 64]) & (1 << (num % 64)));
-    return (page->bitmap[num / 64]) & (1 << (num % 64));
+    return (page->bitmap[num / 64]) & ((uint64_t)1 << (num % 64));
 }
 void setUnit(page_t* page, int num, bool b)
 {
