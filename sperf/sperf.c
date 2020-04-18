d49f4ec7160a753249ffb94c6a6da879d3b713f9
tracer-nju <tracer@nju.edu.cn>
2020-04-19 00:36:26 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: b54bebe2e6dd4c17b7e2bac9c6c54b0c   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  00:36:26 up 48 min,  1 user,  load average: 0.84, 0.76, 0.62

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -5,24 +5,20 @@
 
 int main(int argc, char *argv[]) {
     char *exec_envp[] = { NULL, NULL, };
-    extern char ** environ;
-    for (char ** i = environ; *i != NULL; i++){
-        if (strncmp(*i, "PATH=", 5) == 0){
-            exec_envp[0] = *i;
-            // strtok(*i + 5, ":");
-            // char* s;
-            // while ((s = strtok(NULL, ":"))) printf("%s\n", s);
-        }
-    }
+    printf("%s\n",getenv("PATH");
+    // extern char ** environ;
+    // for (char ** i = environ; *i != NULL; i++){
+    //     if (strncmp(*i, "PATH=", 5) == 0)
+    //         exec_envp[0] = *i;
 
-    char* exec_argv[] = {
-        "strace",
-        NULL,
-        NULL,
-    };
-    exec_argv[1] = argv[1];
+    // char* exec_argv[] = {
+    //     "strace",
+    //     NULL,
+    //     NULL,
+    // };
+    // exec_argv[1] = argv[1];
 
-    execve("/usr/bin/strace", exec_argv, exec_envp);
+    // execve("/usr/bin/strace", exec_argv, exec_envp);
     // perror(argv[0]);
     // exit(EXIT_FAILURE);
 }
