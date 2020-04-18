0a7db7dca417998d0b151b9fe947258ed7309979
tracer-nju <tracer@nju.edu.cn>
2020-04-19 02:57:10 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: b54bebe2e6dd4c17b7e2bac9c6c54b0c   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  02:57:10 up  3:09,  1 user,  load average: 0.53, 0.38, 0.42

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -59,7 +59,7 @@ int main(int argc, char* argv[])
         for (int i = 1; i <= 1000; i++){
             fgets(s, 512, stdin);
             printf("%d\n", i);
-            //puts(s);
+            puts(s);
         }
         printf("HSHSHHSHSHSHS\n");
     }
