c41b8571ee24291c409c49706804626d192b10af
tracer-nju <tracer@nju.edu.cn>
2020-04-19 01:48:27 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: b54bebe2e6dd4c17b7e2bac9c6c54b0c   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  01:48:27 up  2:00,  1 user,  load average: 0.84, 0.69, 0.57

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[])
 {
-    char* exec_argv[argc+20] = {"strace","-T",NULL,NULL,};
+    char* exec_argv[] = {"strace","-T",NULL,NULL,};
     exec_argv[2] = argv[1];
     char *exec_envp[] = { NULL, NULL, };
 
@@ -24,10 +24,17 @@ int main(int argc, char* argv[])
             break;
         }
 
-    __pid_t pid = fork();
+    int pipe_fd[2];
+    if (pipe(pipe_fd)<0){
+        printf("pipe create error\n");
+        assert(0);
+    }
+
+        __pid_t pid = fork();
     if(pid==0){ //children
         for (int i = 0; i < 32;i++)
             if(currenetPaths[i]){
+                freopen(pipe_fd[1], 'w', stderr);
                 char* newLoc = malloc(strlen(currenetPaths[i]) + 10);
                 strcpy(newLoc, currenetPaths[i]);
                 strcat(newLoc, "/strace");
