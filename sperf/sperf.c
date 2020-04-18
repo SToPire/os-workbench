9c119da46086f52d8602e74ac8741c22d73bc0fe
tracer-nju <tracer@nju.edu.cn>
2020-04-19 03:51:04 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: b54bebe2e6dd4c17b7e2bac9c6c54b0c   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  03:51:04 up  4:03,  1 user,  load average: 0.46, 0.61, 0.59

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include<string.h>
+#include <string.h>
 #include <sys/wait.h>
 
-typedef struct __node{
+typedef struct __node {
     char name[64];
     double t;
-}node;
+} node;
 
 int main(int argc, char* argv[])
 {
-    char* exec_argv[argc+2];
+    char* exec_argv[argc + 2];
     exec_argv[0] = "strace";
     exec_argv[1] = "-T";
-    for (int i = 1; i < argc;++i)
+    for (int i = 1; i < argc; ++i)
         exec_argv[i + 1] = argv[i];
-    exec_argv[argc+1]=NULL;
+    exec_argv[argc + 1] = NULL;
 
-    char *exec_envp[] = { NULL, NULL, };
+    char* exec_envp[] = {
+        NULL,
+        NULL,
+    };
 
-    char* currenetPaths[32]={NULL};
+    char* currenetPaths[32] = {NULL};
 
     extern char** environ;
-    for (char ** i = environ; *i != NULL; i++)
-        if (strncmp(*i, "PATH=", 5) == 0){
+    for (char** i = environ; *i != NULL; i++)
+        if (strncmp(*i, "PATH=", 5) == 0) {
             exec_envp[0] = *i;
             char* tmp = malloc(strlen(*i));
             strcpy(tmp, *i + 5);
@@ -36,15 +39,15 @@ int main(int argc, char* argv[])
         }
 
     int pipe_fd[2];
-    if (pipe(pipe_fd)<0){
+    if (pipe(pipe_fd) < 0) {
         printf("pipe create error\n");
         return -1;
     }
 
-        __pid_t pid = fork();
-    if(pid==0){ //children
-        for (int i = 0; i < 32;i++)
-            if(currenetPaths[i]){
+    __pid_t pid = fork();
+    if (pid == 0) {  //children
+        for (int i = 0; i < 32; i++)
+            if (currenetPaths[i]) {
                 dup2(pipe_fd[1], STDERR_FILENO);
                 freopen("/dev/null", "w", stdout);
                 char* newLoc = malloc(strlen(currenetPaths[i]) + 10);
@@ -52,19 +55,23 @@ int main(int argc, char* argv[])
                 strcat(newLoc, "/strace");
                 execve(newLoc, exec_argv, exec_envp);
             }
-    }else{
+    } else {
         sleep(1);
         dup2(pipe_fd[0], STDIN_FILENO);
         //waitpid(pid,0,0);
         char s[512];
         node stat[128];
+        for (int i = 0; i < 128; i++) {
+            stat[i].t = 0.0;
+            strcpy(stat[i].name, "");
+        }
         memset(stat, 0, sizeof(stat));
         double tot = 0.0;
         while (fgets(s, 512, stdin)) {
             int i2 = strlen(s), i3 = strlen(s);
             while (s[i2] != '<' && i2 >= 0) --i2;
             while (s[i3] != '>' && i3 >= 0) --i3;
-            if (i2<0 || i3<0) break;
+            if (i2 < 0 || i3 < 0) break;
             char ts[64];
             int tscnt = 0;
             for (int i = i2 + 1; i < i3; i++) ts[tscnt++] = s[i];
@@ -77,29 +84,29 @@ int main(int argc, char* argv[])
             for (int i = 0; i < i1; ++i) name[i] = s[i];
             name[i1] = '\0';
 
-            for (int i = 0; i < 127;++i){
-              if(strcmp(stat[i].name,name)==0){
-                  printf("asdasdasdasdssssssssssssssssssss");
-                  stat[i].t += t;
-                  break;
-              }else if(stat[i].name == NULL){
-                  printf("asdasdasdasdssssssssssssssssssss");
+            for (int i = 0; i < 127; ++i) {
+                if (strcmp(stat[i].name, name) == 0) {
+                    printf("asdasdasdasdssssssssssssssssssss");
+                    stat[i].t += t;
+                    break;
+                } else if (stat[i].name == "") {
+                    printf("asdasdasdasdssssssssssssssssssss");
 
-                  strcpy(stat[i].name, name);
-                  stat[i].t += t;
-                  break;
-              }
+                    strcpy(stat[i].name, name);
+                    stat[i].t += t;
+                    break;
+                }
             }
             tot += t;
 
             if (strcmp(s, "+++ exited with 0 +++\n") == 0) break;
         }
-        for (int i = 0; i < 128;i++){
-          if(stat[i].name){
-              printf("%s:%f\n", stat[i].name, stat[i].t);
-          }
+        for (int i = 0; i < 128; i++) {
+            if (stat[i].name) {
+                printf("%s:%f\n", stat[i].name, stat[i].t);
+            }
         }
-            printf("HSHSHHSHSHSHS\n");
+        printf("HSHSHHSHSHSHS\n");
     }
     // perror(argv[0]);
     // exit(EXIT_FAILURE);
