36456dd1336c491392baf6b5749f27267e3cfb9d
tracer-nju <tracer@nju.edu.cn>
2020-04-19 01:00:57 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: b54bebe2e6dd4c17b7e2bac9c6c54b0c   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  01:00:57 up  1:13,  1 user,  load average: 1.00, 0.61, 0.57

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,16 +3,29 @@
 #include <unistd.h>
 #include<string.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
     char* exec_argv[] = {"strace",NULL,NULL,};
-    char *exec_envp[] = { NULL, NULL, };
-    extern char ** environ;
-    for (char ** i = environ; *i != NULL; i++)
-        if (strncmp(*i, "PATH=", 5) == 0)
-            exec_envp[0] = *i;
     exec_argv[1] = argv[1];
+    char *exec_envp[] = { NULL, NULL, };
 
-    execve("/usr/bin/strace", exec_argv, exec_envp);
+    char* currenetPaths[20];
+
+    extern char** environ;
+    for (char ** i = environ; *i != NULL; i++)
+        if (strncmp(*i, "PATH=", 5) == 0){
+            exec_envp[0] = *i;
+            strtok(*i + 5, ":");
+            char* s;
+            int ii = 0;
+            while ((currenetPaths[ii++] = strtok(NULL, ":")))
+                ;
+        }
+
+    for (int ii = 0; ii < 20;ii++){
+        if (currenetPaths[ii]) printf("%s\n", currenetPaths[ii]);
+    }
+        execve("/usr/bin/strace", exec_argv, exec_envp);
     // perror(argv[0]);
     // exit(EXIT_FAILURE);
 }
