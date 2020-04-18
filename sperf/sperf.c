b95ecd337e4a0737df4d47e024d13d2f01bc670b
tracer-nju <tracer@nju.edu.cn>
2020-04-19 00:29:39 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: b54bebe2e6dd4c17b7e2bac9c6c54b0c   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  00:29:39 up 42 min,  1 user,  load average: 1.36, 0.90, 0.59

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include<string.h>
 
 int main(int argc, char *argv[]) {
     extern char ** environ;
     char** i;
-    for (i = environ; *i != NULL; i++) printf("%s\n", *i);
+    for (i = environ; *i != NULL; i++){
+        if (strncmp(*i, "PATH=", 5) == 0) printf("%s\n", *i);
+    }
 
     char* exec_argv[] = {
         "strace",
@@ -14,8 +17,7 @@ int main(int argc, char *argv[]) {
     };
     char *exec_envp[] = { "PATH=/bin", NULL, };
     exec_argv[1] = argv[1];
-    //execve("strace",          exec_argv, exec_envp);
-    //execve("/bin/strace",     exec_argv, exec_envp);
+
     execve("/usr/bin/strace", exec_argv, exec_envp);
     // perror(argv[0]);
     // exit(EXIT_FAILURE);
