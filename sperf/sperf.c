2e8dc3412a32dc53aff1c81f4fd096c6a02c006e
tracer-nju <tracer@nju.edu.cn>
2020-04-19 00:20:50 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: b54bebe2e6dd4c17b7e2bac9c6c54b0c   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  00:20:50 up 33 min,  1 user,  load average: 0.44, 0.34, 0.26

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -5,10 +5,11 @@
 int main(int argc, char *argv[]) {
     //printf("%s\n", argv[1]);
 
-    char *exec_argv[] = { "strace", "ls", NULL, };
+    char *exec_argv[] = { "strace", NULL, NULL, };
     char *exec_envp[] = { "PATH=/bin", NULL, };
+    exec_argv[1] = argv[1];
     //execve("strace",          exec_argv, exec_envp);
-     //execve("/bin/strace",     exec_argv, exec_envp);
+    //execve("/bin/strace",     exec_argv, exec_envp);
     execve("/usr/bin/strace", exec_argv, exec_envp);
     // perror(argv[0]);
     // exit(EXIT_FAILURE);
