b78541ea7d88704e58614c02433b82775cf61e87
tracer-nju <tracer@nju.edu.cn>
2020-04-19 00:35:22 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: b54bebe2e6dd4c17b7e2bac9c6c54b0c   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  00:35:23 up 47 min,  1 user,  load average: 0.65, 0.72, 0.60

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -4,12 +4,14 @@
 #include<string.h>
 
 int main(int argc, char *argv[]) {
+    char *exec_envp[] = { NULL, NULL, };
     extern char ** environ;
     for (char ** i = environ; *i != NULL; i++){
         if (strncmp(*i, "PATH=", 5) == 0){
-            strtok(*i + 5, ":");
-            char* s;
-            while ((s = strtok(NULL, ":"))) printf("%s\n", s);
+            exec_envp[0] = *i;
+            // strtok(*i + 5, ":");
+            // char* s;
+            // while ((s = strtok(NULL, ":"))) printf("%s\n", s);
         }
     }
 
@@ -18,7 +20,6 @@ int main(int argc, char *argv[]) {
         NULL,
         NULL,
     };
-    char *exec_envp[] = { "PATH=/bin", NULL, };
     exec_argv[1] = argv[1];
 
     execve("/usr/bin/strace", exec_argv, exec_envp);
