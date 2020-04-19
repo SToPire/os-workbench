6e3f3996f3b4b9e958541f9eff80fc2f8b18b230
tracer-nju <tracer@nju.edu.cn>
2020-04-19 15:25:55 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 3559b62d6a504fb4a72a42eb304f1ff9   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  15:25:55 up  2:08,  1 user,  load average: 0.28, 0.44, 0.45

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -111,11 +111,11 @@ int main(int argc, char* argv[])
                     }
                 }
                 tot += t;
-                if (++cnt == 1000){
-                    f = 1;
-                    break;
-                }
-                f = 0;
+                // if (++cnt == 1000){
+                //     f = 1;
+                //     break;
+                // }
+                // f = 0;
             }
             for (int i = 0; i < 128; i++) {
                 if (strcmp(stat[i].name, "") != 0) {
@@ -125,7 +125,7 @@ int main(int argc, char* argv[])
             for (int i = 1; i <= 80; i++) putc(0, stdout);
             puts("==========================");
             fflush(stdout);
-            if (f) goto l;
+            //if (f) goto l;
 
         }
     }
