12b586474512230eb339d4a082ad143e06d80f8c
tracer-nju <tracer@nju.edu.cn>
2020-04-19 15:37:45 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 3559b62d6a504fb4a72a42eb304f1ff9   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  15:37:45 up  2:20,  1 user,  load average: 1.24, 0.80, 0.59

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -83,49 +83,49 @@ int main(int argc, char* argv[])
         {
             while (1) {
                 if (fgets(s, sizeof(s), stdin) == NULL && waitpid(pid, &status, WNOHANG) == pid) break;
-                //printf("%s\n", s);
+                printf("%s\n", s);
 
-                int i2 = strlen(s), i3 = strlen(s);
-                while (s[i2] != '<' && i2 >= 0) --i2;
-                while (s[i3] != '>' && i3 >= 0) --i3;
-                if (i2 < 0 || i3 < 0) break;
-                char ts[64];
-                int tscnt = 0;
-                for (int i = i2 + 1; i < i3; i++) ts[tscnt++] = s[i];
-                ts[tscnt] = '\0';
-                double t = strtod(ts, 0);
+                // int i2 = strlen(s), i3 = strlen(s);
+                // while (s[i2] != '<' && i2 >= 0) --i2;
+                // while (s[i3] != '>' && i3 >= 0) --i3;
+                // if (i2 < 0 || i3 < 0) break;
+                // char ts[64];
+                // int tscnt = 0;
+                // for (int i = i2 + 1; i < i3; i++) ts[tscnt++] = s[i];
+                // ts[tscnt] = '\0';
+                // double t = strtod(ts, 0);
 
-                int i1 = 0;
-                char name[64];
-                while (s[i1] != '(') ++i1;
-                for (int i = 0; i < i1; ++i) name[i] = s[i];
-                name[i1] = '\0';
+                // int i1 = 0;
+                // char name[64];
+                // while (s[i1] != '(') ++i1;
+                // for (int i = 0; i < i1; ++i) name[i] = s[i];
+                // name[i1] = '\0';
 
-                for (int i = 0; i < 127; ++i) {
-                    if (strcmp(stat[i].name, name) == 0) {
-                        stat[i].t += t;
-                        break;
-                    } else if (strcmp(stat[i].name, "") == 0) {
-                        strcpy(stat[i].name, name);
-                        stat[i].t += t;
-                        break;
-                    }
-                }
-                tot += t;
+                // for (int i = 0; i < 127; ++i) {
+                //     if (strcmp(stat[i].name, name) == 0) {
+                //         stat[i].t += t;
+                //         break;
+                //     } else if (strcmp(stat[i].name, "") == 0) {
+                //         strcpy(stat[i].name, name);
+                //         stat[i].t += t;
+                //         break;
+                //     }
+                // }
+                // tot += t;
                 // if (++cnt == 1000){
                 //     f = 1;
                 //     break;
                 // }
                 // f = 0;
             }
-            for (int i = 0; i < 128; i++) {
-                if (strcmp(stat[i].name, "") != 0) {
-                    printf("%s(%.0f%%)\n", stat[i].name, 100 * stat[i].t / tot);
-                }
-            }
-            for (int i = 1; i <= 80; i++) putc(0, stdout);
-            puts("==========================");
-            fflush(stdout);
+            // for (int i = 0; i < 128; i++) {
+            //     if (strcmp(stat[i].name, "") != 0) {
+            //         printf("%s(%.0f%%)\n", stat[i].name, 100 * stat[i].t / tot);
+            //     }
+            // }
+            // for (int i = 1; i <= 80; i++) putc(0, stdout);
+            // puts("==========================");
+            // fflush(stdout);
             //if (f) goto l;
 
         }
