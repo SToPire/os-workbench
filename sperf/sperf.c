0c9bb09a583b88a0f0e1537b108844f9a0e5efa7
tracer-nju <tracer@nju.edu.cn>
2020-04-19 13:42:51 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 3559b62d6a504fb4a72a42eb304f1ff9   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  13:42:51 up 25 min,  1 user,  load average: 0.41, 0.52, 0.44

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -46,8 +46,8 @@ int main(int argc, char* argv[])
     if (pid == 0) {  //children
         for (int i = 0; i < 32; i++)
             if (currenetPaths[i]) {
-                //dup2(pipe_fd[1], STDERR_FILENO);
-                //freopen("/dev/null", "w", stdout);
+                dup2(pipe_fd[1], STDERR_FILENO);
+                freopen("/dev/null", "w", stdout);
                 char* newLoc = malloc(strlen(currenetPaths[i]) + 10);
                 strcpy(newLoc, currenetPaths[i]);
                 strcat(newLoc, "/strace");
