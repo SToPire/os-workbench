0a9390075a8aa2160504549c5dba0184868937eb
tracer-nju <tracer@nju.edu.cn>
2020-04-25 16:40:08 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: e53f63d985fb4ccc92e5ed3693599502   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  16:40:08 up  1:53,  1 user,  load average: 0.91, 0.62, 0.56

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -22,7 +22,7 @@ int main(int argc, char* argv[])
         char Cname[32],Soname[32];
         sprintf(Cname, "/tmp/crepl-%d.c", ++FILECNT);
         sprintf(Soname, "/tmp/crepl-%d.so", FILECNT);
-        FILE* fp = fopen(Cname, "r");
+        FILE* fp = fopen(Cname, "w+");
         //fputs("int f(){return 233;}", fp);
 
         char* exec_argv[] = {"gcc", "-fPIC", "-shared", Cname, "-o", Soname, NULL};
