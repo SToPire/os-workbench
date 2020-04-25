b42fea1d1ebdb5af63552946a3644f7748ce54ad
tracer-nju <tracer@nju.edu.cn>
2020-04-25 19:47:29 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: e53f63d985fb4ccc92e5ed3693599502   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  19:47:29 up  5:00,  1 user,  load average: 0.36, 0.45, 0.31

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -31,20 +31,10 @@ int main(int argc, char* argv[])
         char wrapper[4096 + 64], wrapper_name[32];
         char fun_name[4096];
         if (strncmp(line, "int", 3) == 0) {
-            // strcpy(fun_name, line + 4);
-            // int i = 0;
-            // for (; fun_name[i] != '(';++i)
-            //     ;
-            // fun_name[i] = '\0';
             strcpy(funcs[funcsCnt++], line);
-
             for (int i = 0; i < funcsCnt; ++i) {
                 fputs(funcs[i], fp);
             }
-            // sprintf(wrapper_name, "__expr_wrapper_%d", FILECNT);
-            // sprintf(wrapper, "int __expr_wrapper_%d(){return %s();}", FILECNT, fun_name);
-            // fputs(wrapper, fp);
-
         } else {
             for (int i = 0; i < funcsCnt; ++i) {
                 fputs(funcs[i], fp);
@@ -60,8 +50,8 @@ int main(int argc, char* argv[])
 
         int pipe_fd[2];
         if (pipe(pipe_fd) < 0) {
-            printf("pipe create error\n");
-            return -1;
+            fprintf(stderr,"pipe create error\n");
+            exit(EXIT_FAILURE);
         }
 
         __pid_t pid = fork();
