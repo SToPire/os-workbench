3e83362797788418d05f125d644547ea4f32feb9
tracer-nju <tracer@nju.edu.cn>
2020-04-25 15:26:20 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: e53f63d985fb4ccc92e5ed3693599502   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  15:26:20 up 39 min,  1 user,  load average: 1.11, 0.77, 0.50

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int main(int argc, char *argv[]) {
-  static char line[4096];
-  while (1) {
-    printf("crepl> ");
-    fflush(stdout);
-    if (!fgets(line, sizeof(line), stdin)) {
-      break;
-    }
-    printf("%s", line);
-    printf("Got %zu chars.\n", strlen(line));  // WTF?
+    printf("%s\n", getenv("PATH"));
+
+    static char line[4096];
+    while (1) {
+        printf("crepl> ");
+        fflush(stdout);
+        if (!fgets(line, sizeof(line), stdin)) {
+            break;
+        }
+        printf("%s", line);
+        printf("Got %zu chars.\n", strlen(line));  // WTF?
   }
 }
