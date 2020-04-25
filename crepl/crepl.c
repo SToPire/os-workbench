9d1da7e786e13cf87759db04bd28988b89b90aa8
tracer-nju <tracer@nju.edu.cn>
2020-04-25 16:11:33 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: e53f63d985fb4ccc92e5ed3693599502   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  16:11:33 up  1:24,  1 user,  load average: 0.95, 0.78, 0.64

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,37 +1,40 @@
 #include <stdio.h>
 #include <string.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/wait.h>
-#include<dlfcn.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <dlfcn.h>
 
+#define ROOT /tmp/
 typedef int (*FUN)();
 int main(int argc, char* argv[])
 {
-    char* exec_argv[] = {"gcc","-fPIC", "-shared", "/tmp/tmp.c", "-o", "/tmp/tmp.so",NULL};
-    __pid_t pid = fork();
-    if(pid==0){
-        execvp("gcc", exec_argv);
-    } else {
-        while(waitpid(pid,NULL,WNOHANG)!=pid)
-            ;
-        void* handle = dlopen("/tmp/tmp.so", RTLD_LAZY);
-        if (!handle){
-            fprintf(stderr, "%s\n", dlerror());
-            exit(EXIT_FAILURE);
+    static char line[4096];
+    while (1) {
+        printf("crepl> ");
+        fflush(stdout);
+        if (!fgets(line, sizeof(line), stdin)) {
+            break;
         }
-        FUN F;
-        F = (FUN)dlsym(handle, "f");
-        printf("output:%d\n", F());
-        // static char line[4096];
-        // while (1) {
-        //     printf("crepl> ");
-        //     fflush(stdout);
-        //     if (!fgets(line, sizeof(line), stdin)) {
-        //         break;
-        //     }
-        //     printf("%s", line);
-        //     printf("Got %zu chars.\n", strlen(line));  // WTF?
-        // }
+        printf("%s", line);
+
+        char* exec_argv[] = {"gcc", "-fPIC", "-shared", "/tmp/tmp.c", "-o", "/tmp/tmp.so", NULL};
+        __pid_t pid = fork();
+        if (pid == 0) {
+            execvp("gcc", exec_argv);
+        } else {
+            while (waitpid(pid, NULL, WNOHANG) != pid)
+                ;
+            void* handle = dlopen("/tmp/tmp.so", RTLD_LAZY);
+            if (!handle) {
+                fprintf(stderr, "%s\n", dlerror());
+                exit(EXIT_FAILURE);
+            }
+            FUN F;
+            F = (FUN)dlsym(handle, "f");
+            printf("output:%d\n", F());
+
+            printf("Got %zu chars.\n", strlen(line));  // WTF?
+        }
     }
 }
