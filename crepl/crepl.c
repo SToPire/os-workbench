ac7a693b35a6d7e415c0f478be0c677a0e9d5e80
tracer-nju <tracer@nju.edu.cn>
2020-04-25 15:42:36 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: e53f63d985fb4ccc92e5ed3693599502   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  15:42:37 up 55 min,  1 user,  load average: 1.05, 0.66, 0.56

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 #include<stdlib.h>
-
+#include<unistd.h>
 int main(int argc, char *argv[]) {
     // char path[256];
     // sprintf(path, "PATH=%s", getenv("PATH"));
     // char* exec_envp[] = {NULL, NULL};
     // exec_envp[0] = path;
 
+    char* exec_argc[] = {"-fPIC", "-shared", "/tmp/tmp.c", "-o", "tmp/tmp.o"};
     __pid_t pid = fork();
     if(pid==0){
-        execvp("gcc", "-fPIC -shared /tmp/tmp.c -o /tmp/tmp.o ");
+        execvp("gcc", exec_argc);
     } else {
         static char line[4096];
         while (1) {
