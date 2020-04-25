acaf2746756fa37f849c75dbefd67472c8b1cd4a
tracer-nju <tracer@nju.edu.cn>
2020-04-25 17:31:59 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: e53f63d985fb4ccc92e5ed3693599502   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  17:31:59 up  2:45,  1 user,  load average: 0.44, 0.45, 0.45

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -54,7 +54,7 @@ int main(int argc, char* argv[])
                 WRAPPER W;
                 W = (WRAPPER)dlsym(handle, wrapper_name);
                 printf("%d\n", W());
-                dlclose(handle);
+                //dlclose(handle);
             }
         }
     }
