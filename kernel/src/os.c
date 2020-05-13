9423d85f3ce154169c9c9ffc11a3095c3b9806a3
tracer-nju <tracer@nju.edu.cn>
2020-05-14 00:31:13 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: f85dd43afd3c48f29b38e20f79f6c1e0   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  00:31:13 up  2:35,  1 user,  load average: 1.01, 0.72, 0.63

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -11,11 +11,11 @@ static void os_run()
     spinlock_t lk;
     if (_cpu() == 0){
         spin_lock(&lk);
-        putstr("0 is holding the lock");
+        putstr("0 is holding the lock\n");
         spin_unlock(&lk);
     } else if (_cpu() == 1) {
         spin_lock(&lk);
-        putstr("1 is holding the lock");
+        putstr("1 is holding the lock\n");
         spin_unlock(&lk);
     }
 }
