873c81d0bfe65e28785343ce984a367e21ef571a
tracer-nju <tracer@nju.edu.cn>
2020-05-13 22:46:02 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: f85dd43afd3c48f29b38e20f79f6c1e0   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  22:46:02 up 50 min,  1 user,  load average: 0.44, 0.26, 0.15

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -3,56 +3,19 @@
 static void os_init()
 {
     pmm->init();
-    kmt->init();
+   // kmt->init();
 }
 //static void* a[10];
 static void os_run()
 {
-    for (const char* s = "Hello World from CPU #*\n"; *s; s++) {
-        _putc(*s == '*' ? '0' + _cpu() : *s);
-    }
-    
-    // printf("%p\n", a);
-    for (int i = 1; i <= 2000; i++) pmm->alloc(2048);
-    for (int i = 1; i <= 2000; i++) pmm->alloc(512);
-    for (int i = 1; i <= 8000; i++) pmm->alloc(4);
-    for (int i = 1; i <= 2000; i++) pmm->alloc(4096);
-    for (int i = 1; i <= 2000; i++) pmm->alloc(1024);
-    
-
-    // if(_cpu()==0){
-    //     a[0] = pmm->alloc(2048);
-    //     a[1] = pmm->alloc(2048);
-    //     for (volatile int i = 1; i <= 1000000; i++)        ;
-    // }
-    // if(_cpu()==1){
-    //     pmm->free(a[0]);
-    //     for (volatile int i = 1; i <= 1000000; i++)
-    //         ;
-    // }
-    // if(_cpu()==0){
-    //     pmm->alloc(2048);
-    //     pmm->alloc(2048);
-    // }
-
-    // if (_cpu() == 1) {
-    //     pmm->free(a[1]);
-    //     pmm->free(a[2]);
-    //     pmm->free(a[3]);
-    //     pmm->free(a[4]);
-    //     pmm->free(a[5]);
-    //     for (volatile int i = 1; i <= 1000000; i++)
-    //         ;
-    // }
-    // if (_cpu() == 0)
-    //     for (volatile int i = 1; i <= 1000000; i++)
-    //         ;
-    // pmm->alloc(2048);
-    // pmm->alloc(2048);
-    // pmm->alloc(2048);
-
-    while (1)
+    _intr_write(1);
+    while(1)
         ;
+}
+
+_Context* os_trap(_Event ev, _Context* context)
+{
+    return context;
 }
 
 MODULE_DEF(os) = {
