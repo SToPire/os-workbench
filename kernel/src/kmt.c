cf8433bf0737f6c7b9ad06fb03be2902847dea7c
tracer-nju <tracer@nju.edu.cn>
2020-05-16 01:21:31 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: e35af0f68af5418eb710905b10f93324   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  01:21:31 up  4:48,  1 user,  load average: 0.35, 0.65, 0.63

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -30,7 +30,7 @@ struct cpu_local {
 
 _Context* scheduler(_Event ev, _Context* _Context)
 {
-    printf("here");
+    //printf("here");
     if (!current) {
         current = TASKS[0];
     } else {
