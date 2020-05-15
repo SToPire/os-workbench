2add874d242725f9a015d5395ef48a2786e0235c
tracer-nju <tracer@nju.edu.cn>
2020-05-16 01:09:45 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: e35af0f68af5418eb710905b10f93324   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  01:09:46 up  4:37,  1 user,  load average: 0.30, 0.52, 0.46

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -11,7 +11,7 @@ int create(task_t* task, const char* name, void (*entry)(void* arg), void* arg)
     spin_lock(&bigLock);
     task->name = name;
     _Area stack = (_Area){&task->context + 1, task + 1};
-   // printf("%p %p\n", &task->context + 1, task + 1);
+   printf("%p %p\n", &task->context + 1, task + 1);
     task->context = _kcontext(stack, entry, arg);
     // task->next = (TASKS_P + 1) % 32;
     task->next = 0;
@@ -37,6 +37,8 @@ _Context* scheduler(_Event ev, _Context* _Context)
     }
     do {
         current = TASKS[current->next];
+       // printf("HERE\n");
+
     } while ((current - TASKS[0]) % _ncpu() != _cpu());
 
     return current->context;
