2477ecf2b2852b731a3d079e87abddaee694fe13
tracer-nju <tracer@nju.edu.cn>
2020-05-15 22:14:14 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: e35af0f68af5418eb710905b10f93324   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  22:14:14 up  1:41,  1 user,  load average: 0.95, 0.57, 0.34

--- kernel/include/kmt.h
+++ kernel/include/kmt.h
@@ -1,9 +1,19 @@
-#include<am.h>
-struct task{
-    struct{
-        char* name;
-        struct task* next;
-        _Context* context;
+#include <am.h>
+typedef struct task task_t;
+
+struct task {
+    union {
+        struct {
+            const char* name;
+            struct task* next;
+            _Context* context;
+        };
+        uint8_t stack[4096];
     };
-    uint8_t stack[4096];
 };
+
+int kmt_create(task_t* task, const char* name, void (*entry)(void* arg), void* arg)
+{
+    task->name = name;
+    return 0;
+}
--- kernel/src/os.c
+++ kernel/src/os.c
@@ -1,24 +1,24 @@
 #include <common.h>
-spinlock_t lk;
+//spinlock_t lk;
 
 static void os_init()
 {
     pmm->init();
    // kmt->init();
-    spin_init(&lk, NULL);  //for test
+    //spin_init(&lk, NULL);  //for test
 }
 static void os_run()
 {
     _intr_write(0);
-    if (_cpu() == 0) {
-        spin_lock(&lk);
-        putstr("0 is holding the lock\n");
-        spin_unlock(&lk);
-    } else if (_cpu() == 1) {
-        spin_lock(&lk);
-        putstr("1 is holding the lock\n");
-        spin_unlock(&lk);
-    }
+    // if (_cpu() == 0) {
+    //     spin_lock(&lk);
+    //     putstr("0 is holding the lock\n");
+    //     spin_unlock(&lk);
+    // } else if (_cpu() == 1) {
+    //     spin_lock(&lk);
+    //     putstr("1 is holding the lock\n");
+    //     spin_unlock(&lk);
+    // }
     while(1)
         ;
 }
