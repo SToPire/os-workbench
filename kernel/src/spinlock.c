23c3cc9d21242adf6b292f5d9068724ccbc8ce21
tracer-nju <tracer@nju.edu.cn>
2020-05-16 02:27:15 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: e35af0f68af5418eb710905b10f93324   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  02:27:15 up  5:54,  1 user,  load average: 1.03, 0.76, 0.67

--- kernel/src/spinlock.c
+++ kernel/src/spinlock.c
@@ -19,7 +19,6 @@ void spin_init(spinlock_t* lk,const char* name)
 void spin_lock(spinlock_t* lk)
 {
     pushcli();
-    printf("%d is approaching:\n", _cpu());
     panic_on(holding(lk), "Trying to hold a lock which already held by this cpu");  
     while (_atomic_xchg(&lk->locked, 1))
         ;
