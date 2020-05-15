596637bdb74dbd74645f584b53d7aaf9ca511b9a
tracer-nju <tracer@nju.edu.cn>
2020-05-16 01:04:47 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: e35af0f68af5418eb710905b10f93324   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  01:04:48 up  4:32,  1 user,  load average: 0.32, 0.40, 0.38

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -20,7 +20,8 @@ static void os_init()
     kmt->init();
     spin_init(&lk, NULL);  //for test
 
-    kmt->create(pmm->alloc(sizeof(task_t)), "th1", th1, NULL);
+    task_t tt;
+    kmt->create(&tt, "th1", th1, NULL);
     //kmt->create(pmm->alloc(sizeof(task_t)), "th2", th2, NULL);
 }
 
