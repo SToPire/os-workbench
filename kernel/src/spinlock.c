edbbd31b129113b45c5a689505f09d4d39588a3e
tracer-nju <tracer@nju.edu.cn>
2020-05-16 02:30:51 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: e35af0f68af5418eb710905b10f93324   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  02:30:51 up  5:58,  1 user,  load average: 1.55, 1.34, 0.94

--- kernel/src/spinlock.c
+++ kernel/src/spinlock.c
@@ -1,6 +1,6 @@
 #include <common.h>
 
-struct _cpustat{
+struct _cpustat {
     int ncli;
     int intena;
 } cpustat[8];
@@ -9,7 +9,7 @@ void pushcli();
 void popcli();
 int holding(spinlock_t* lk);
 
-void spin_init(spinlock_t* lk,const char* name)
+void spin_init(spinlock_t* lk, const char* name)
 {
     lk->locked = 0;
     lk->cpu = 0;
@@ -19,19 +19,22 @@ void spin_init(spinlock_t* lk,const char* name)
 void spin_lock(spinlock_t* lk)
 {
     pushcli();
-    panic_on(holding(lk), "Trying to hold a lock which already held by this cpu");
-    //if (holding(lk)) printf("Tring to relock:cpu%d\n", _cpu());
+    //panic_on(holding(lk), "Trying to hold a lock which already held by this cpu");
+    if (holding(lk)) {
+        printf("Tring to relock:cpu%d\n", _cpu());
+       _halt(1);
+    }
     while (_atomic_xchg(&lk->locked, 1))
         ;
     lk->cpu = _cpu();
 
-     printf("spin_lock from cpu %d\n", _cpu());
+    printf("spin_lock from cpu %d\n", _cpu());
 }
 
 void spin_unlock(spinlock_t* lk)
 {
-     printf("spin_unlock from cpu %d\n", _cpu());
-    
+    printf("spin_unlock from cpu %d\n", _cpu());
+
     panic_on(!holding(lk), "releasing a unheld lock");
     lk->cpu = 0;
     _atomic_xchg(&lk->locked, 0);
@@ -51,7 +54,7 @@ void pushcli()
 {
     int i = _intr_read();
     _intr_write(0);
-    if(cpustat[_cpu()].ncli==0)
+    if (cpustat[_cpu()].ncli == 0)
         cpustat[_cpu()].intena = i;
     ++cpustat[_cpu()].ncli;
 }
@@ -60,7 +63,6 @@ void popcli()
 {
     panic_on(_intr_read() == 1, "interrupt is open while lock is held");
     panic_on((--cpustat[_cpu()].ncli) < 0, "ncli < 0");
-    if(cpustat[_cpu()].ncli==0 && cpustat[_cpu()].intena)
+    if (cpustat[_cpu()].ncli == 0 && cpustat[_cpu()].intena)
         _intr_write(1);
 }
-
