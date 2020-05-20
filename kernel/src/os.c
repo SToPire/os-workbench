72d7a8fa77438ece48bb0aa183ce212976565902
tracer-nju <tracer@nju.edu.cn>
2020-05-20 23:52:12 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: c4129bdb95794030a93ed924702619d8   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  23:52:12 up 35 min,  1 user,  load average: 0.41, 0.51, 0.44

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -6,6 +6,7 @@ sem_t empty, fill;
 void producer(void* arg)
 {
     while (1) {
+        printf("producer:\n");
         P(&empty);
         _putc('(');
         V(&fill);
@@ -14,6 +15,8 @@ void producer(void* arg)
 void consumer(void* arg)
 {
     while (1) {
+        printf("consumer:\n");
+
         P(&fill);
         _putc(')');
         V(&empty);
