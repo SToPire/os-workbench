fbf1bb874cc55a482df4ce4bcfaa5c7c2ca45204
tracer-nju <tracer@nju.edu.cn>
2020-05-21 18:19:51 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 44d3a7ab47d44e3a94823b3ba4682f74   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  18:19:51 up 39 min,  1 user,  load average: 0.61, 0.78, 0.63

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -1,62 +1,43 @@
 #include <common.h>
 spinlock_t trapLock;
 spinlock_t lk;
-// sem_t empty, fill;
-// #define P kmt->sem_wait
-// #define V kmt->sem_signal
-// void producer(void* arg)
-// {
-//     while (1) {
-//         P(&empty);
-//         _putc('(');
-//         V(&fill);
-//     }
-// }
-// void consumer(void* arg)
-// {
-//     while (1) {
-//         P(&fill);
-//         _putc(')');
-//         V(&empty);
-//     }
-// }
 
-void th1()
-{
-    //assert(_cpu() == 0);
-    while (1) {
-        spin_lock(&lk);
-        assert(_intr_read() == 0);
-        printf("This is th1 running in CPU %d!\n",_cpu());
-        spin_unlock(&lk);
-        for (volatile int i = 1; i < 100000; i++)
-            ;
-    }
-}
-void th2()
-{
-    //assert(_cpu() == 1);
-    while (1) {
-        spin_lock(&lk);
-        assert(_intr_read() == 0);
-        printf("This is th2 running in CPU %d!\n", _cpu());
-        spin_unlock(&lk);
-        for (volatile int i = 1; i < 100000; i++)
-            ;
-    }
-}
-void th3()
-{
-    //assert(_cpu() == 0);
-    while (1) {
-        spin_lock(&lk);
-        assert(_intr_read() == 0);
-        printf("This is th3 running in CPU %d!\n", _cpu());
-        spin_unlock(&lk);
-        for (volatile int i = 1; i < 100000; i++)
-            ;
-    }
-}
+// void th1()
+// {
+//     //assert(_cpu() == 0);
+//     while (1) {
+//         spin_lock(&lk);
+//         assert(_intr_read() == 0);
+//         printf("This is th1 running in CPU %d!\n",_cpu());
+//         spin_unlock(&lk);
+//         for (volatile int i = 1; i < 100000; i++)
+//             ;
+//     }
+// }
+// void th2()
+// {
+//     //assert(_cpu() == 1);
+//     while (1) {
+//         spin_lock(&lk);
+//         assert(_intr_read() == 0);
+//         printf("This is th2 running in CPU %d!\n", _cpu());
+//         spin_unlock(&lk);
+//         for (volatile int i = 1; i < 100000; i++)
+//             ;
+//     }
+// }
+// void th3()
+// {
+//     //assert(_cpu() == 0);
+//     while (1) {
+//         spin_lock(&lk);
+//         assert(_intr_read() == 0);
+//         printf("This is th3 running in CPU %d!\n", _cpu());
+//         spin_unlock(&lk);
+//         for (volatile int i = 1; i < 100000; i++)
+//             ;
+//     }
+// }
 static void os_init()
 {
     pmm->init();
@@ -64,23 +45,16 @@ static void os_init()
 
     spin_init(&trapLock, NULL);
 
-    spin_init(&lk, NULL);
+    //spin_init(&lk, NULL);
     //sem_init(&sema, NULL, 0);
 
-    task_t* t1 = pmm->alloc(sizeof(task_t));
-    task_t* t2 = pmm->alloc(sizeof(task_t));
-    task_t* t3 = pmm->alloc(sizeof(task_t));
+    // task_t* t1 = pmm->alloc(sizeof(task_t));
+    // task_t* t2 = pmm->alloc(sizeof(task_t));
+    // task_t* t3 = pmm->alloc(sizeof(task_t));
 
-    kmt->create(t1, "th1", th1, NULL);
-    kmt->create(t2, "th2", th2, NULL);
-    kmt->create(t3, "th3", th3, NULL);
-
-    // kmt->sem_init(&empty, "empty", 5);  // 缓冲区大小为 5
-    // kmt->sem_init(&fill, "fill", 0);
-    // for (int i = 0; i < 4; i++)  // 4 个生产者
-    //     kmt->create(pmm->alloc(sizeof(task_t)), "producer", producer, NULL);
-    // for (int i = 0; i < 5; i++)  // 5 个消费者
-    //     kmt->create(pmm->alloc(sizeof(task_t)), "consumer", consumer, NULL);
+    // kmt->create(t1, "th1", th1, NULL);
+    // kmt->create(t2, "th2", th2, NULL);
+    // kmt->create(t3, "th3", th3, NULL);
 }
 
 static void os_run()
@@ -92,7 +66,7 @@ static void os_run()
 
 _Context* os_trap(_Event ev, _Context* context)
 {
-    spin_lock(&trapLock);
+    //spin_lock(&trapLock);
     _Context* next = NULL;
     for (int i = 0; i <= MAX_INTR;i++) {
         if (INTR[i].valid == 1 && (INTR[i].event == _EVENT_NULL || INTR[i].event == ev.event)) {
@@ -102,7 +76,7 @@ _Context* os_trap(_Event ev, _Context* context)
         }
     }
     panic_on(!next, "returning NULL context");
-    spin_unlock(&trapLock);
+    //spin_unlock(&trapLock);
     return next;
 }
 void on_irq(int seq, int event, handler_t handler)
