b29c9199a477fdd72c4ae53fc25910b6ec881872
tracer-nju <tracer@nju.edu.cn>
2020-05-21 23:47:17 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: a5c0793d4a8c48ee99f4c07c059fcc1b   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  23:47:17 up  3:00,  1 user,  load average: 0.57, 0.68, 0.54

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -81,7 +81,7 @@ void teardown(task_t* task)
 
 _Context* scheduler(_Event ev, _Context* _Context)
 {
-    printf("%d %d %d\n", TASKS[0]->status, TASKS[1]->status, TASKS[2]->status);
+    printf("%d %d %d\n", TASKS[0]->sticky, TASKS[1]->sticky, TASKS[2]->sticky);
     if (cpu_local[_cpu()].sticky != NULL) {
         printf("here\n");
         cpu_local[_cpu()].sticky->sticky = 0;
