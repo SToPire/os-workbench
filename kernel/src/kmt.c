af1a589df061a9697da329447ebdf0380a73001f
tracer-nju <tracer@nju.edu.cn>
2020-05-22 00:55:15 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: a5c0793d4a8c48ee99f4c07c059fcc1b   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  00:55:15 up  4:08,  1 user,  load average: 2.18, 1.23, 0.83

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -67,54 +67,54 @@ void teardown(task_t* task)
     kmt->spin_unlock(&bigKmtLock);
 }
 
-// _Context* scheduler(_Event ev, _Context* _Context)
-// {
-//     if (!current) {
-//         current = TASKS[TASKS_HEAD];
-//     } else {
-//         current->context = _Context;
-//     }
-//     do {
-//         current = TASKS[current->next];
-//     } while ((current->num) % _ncpu() != _cpu() || TASKS[current->num]->status != READY);
-
-//     return current->context;
-// }
 _Context* scheduler(_Event ev, _Context* _Context)
 {
-    // spin_lock(&slock);
-    if (cpu_local[_cpu()].sticky != NULL) {
-        cpu_local[_cpu()].sticky->sticky = 0;
-        //printf("first if        %d %d %d\n", cpu_local[_cpu()].sticky->num, cpu_local[_cpu()].sticky->status, cpu_local[_cpu()].sticky->sticky);
-        cpu_local[_cpu()].sticky = NULL;
-    }
-    task_t* i = TASKS[TASKS_PTR];
-    //printf("%d %d ptr:%d\n", TASKS[0]->status,TASKS[0]->sticky,TASKS_PTR);
-    if(current){
-        current->status = READY;
-    }
-    for (int j = 0; j < MAX_TASKS; j++, i = TASKS[i->next]) {
-        //printf("%d %d %d\n", i->num, i->status, i->sticky);
-        if (i->status == READY && i->sticky == 0) {
-            TASKS_PTR = TASKS[i->next]->num;
-            break;
-        }
-        assert(j <= 20);
-    }
-    if (current) {
-        //printf("second if\n");
+    if (!current) {
+        current = TASKS[TASKS_HEAD];
+    } else {
         current->context = _Context;
-        current->sticky = 1;
-        cpu_local[_cpu()].sticky = current;
     }
-    current = i;
-    current->status = RUNNING;
-    //printf("i:%d\n", i->num);
-    //printf("%d %d %d\n", TASKS[0]->sticky, TASKS[1]->sticky, TASKS[2]->sticky);
-    // spin_unlock(&slock);
+    do {
+        current = TASKS[current->next];
+    } while ((current->num) % _ncpu() != _cpu() || TASKS[current->num]->status != READY);
 
     return current->context;
 }
+// _Context* scheduler(_Event ev, _Context* _Context)
+// {
+//     // spin_lock(&slock);
+//     if (cpu_local[_cpu()].sticky != NULL) {
+//         cpu_local[_cpu()].sticky->sticky = 0;
+//         //printf("first if        %d %d %d\n", cpu_local[_cpu()].sticky->num, cpu_local[_cpu()].sticky->status, cpu_local[_cpu()].sticky->sticky);
+//         cpu_local[_cpu()].sticky = NULL;
+//     }
+//     task_t* i = TASKS[TASKS_PTR];
+//     //printf("%d %d ptr:%d\n", TASKS[0]->status,TASKS[0]->sticky,TASKS_PTR);
+//     if(current){
+//         current->status = READY;
+//     }
+//     for (int j = 0; j < MAX_TASKS; j++, i = TASKS[i->next]) {
+//         //printf("%d %d %d\n", i->num, i->status, i->sticky);
+//         if (i->status == READY && i->sticky == 0) {
+//             TASKS_PTR = TASKS[i->next]->num;
+//             break;
+//         }
+//         assert(j <= 20);
+//     }
+//     if (current) {
+//         //printf("second if\n");
+//         current->context = _Context;
+//         current->sticky = 1;
+//         cpu_local[_cpu()].sticky = current;
+//     }
+//     current = i;
+//     current->status = RUNNING;
+//     //printf("i:%d\n", i->num);
+//     //printf("%d %d %d\n", TASKS[0]->sticky, TASKS[1]->sticky, TASKS[2]->sticky);
+//     // spin_unlock(&slock);
+
+//     return current->context;
+// }
 
 MODULE_DEF(kmt) = {
     .init = kmt_init,
