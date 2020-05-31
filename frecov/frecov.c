bb22207ac01213e2376283f2a3e4cf0c4fd84b60
tracer-nju <tracer@nju.edu.cn>
2020-05-31 21:42:26 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: bbafe007783f4b3fb9b242fd8799616a   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  21:42:26 up  5:40,  1 user,  load average: 1.23, 0.54, 0.30

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,2 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+typedef __uint8_t u8;
+typedef __uint16_t u16;
+typedef __uint32_t u32;
+
+struct fat_header {
+    u8 BS_jmpBoot[3];
+    u8 BS_OEMName[8];
+    u32 BPB_BytsPerSec : 16;
+    u32 BPB_SecPerClus : 8;
+    
+    u8 padding[420];
+    u16 signature;
+} __attribute__((packed));
+
 int main(int argc, char *argv[]) {
 }
