b8f6af83c9642b98ca389477735941694496590a
tracer-nju <tracer@nju.edu.cn>
2020-05-31 22:48:15 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: bbafe007783f4b3fb9b242fd8799616a   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  22:48:15 up  6:46,  1 user,  load average: 0.79, 0.63, 0.53

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -55,7 +55,7 @@ void* Mmap(char* name)
 int main(int argc, char *argv[]) {
     void * ImgPtr = Mmap(argv[1]);
     fat_header_t* fhp = (fat_header_t*)ImgPtr;
-    printf("%u\n", fhp->BPB_FATSz32);
+    printf("%u\n", fhp->BPB_NumFATs);
 
     void* DataPtr = ImgPtr + fhp->BPB_BytsPerSec * (fhp->BPB_RsvdSecCnt + fhp->BPB_NumFATs * fhp->BPB_FATSz32);
 }
