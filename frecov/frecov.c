a8806be6a4c0ed4e466df13074e5514eedc2f733
tracer-nju <tracer@nju.edu.cn>
2020-06-04 18:03:18 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: ad7a33584ec04f9ea218c8589931d82e   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  18:03:18 up  2:19,  1 user,  load average: 0.67, 0.41, 0.31

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -94,7 +94,7 @@ int main(int argc, char* argv[])
         int nameptr = 0;
 
         if (left != right) {
-            for (lEntry_t* i = (lEntry_t*)(right - 1); i >= left; i--) {
+            for (lEntry_t* i = (lEntry_t*)(right - 1); i >= (lEntry_t*)left; i--) {
                 for (int j = 0; j < 5; i++) name[nameptr++] = (char)(i->LDIR_Name1[j]);
                 for (int j = 0; j < 6; i++) name[nameptr++] = (char)(i->LDIR_Name2[j]);
                 for (int j = 0; j < 2; i++) name[nameptr++] = (char)(i->LDIR_Name3[j]);
