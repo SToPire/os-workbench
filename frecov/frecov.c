ec7dfb7852c3fae7de2ee3791b1810e00dfe6786
tracer-nju <tracer@nju.edu.cn>
2020-06-04 18:06:52 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: ad7a33584ec04f9ea218c8589931d82e   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  18:06:53 up  2:23,  1 user,  load average: 0.55, 0.52, 0.38

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -91,11 +91,11 @@ int main(int argc, char* argv[])
         sEntry_t* right = left;
         while (right->DIR_Attr != 0x20) ++right;
         printf("%ld\n", (long)(right - left));
-        printf("fuuck\n");
         char name[128];
         int nameptr = 0;
 
         if (left != right) {
+        printf("fuuck\n");
             for (lEntry_t* i = (lEntry_t*)(right - 1); i >= (lEntry_t*)left; i--) {
                 for (int j = 0; j < 5; i++) name[nameptr++] = (char)(i->LDIR_Name1[j]);
                 for (int j = 0; j < 6; i++) name[nameptr++] = (char)(i->LDIR_Name2[j]);
