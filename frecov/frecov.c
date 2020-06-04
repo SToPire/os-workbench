93da10772b2353920f68a9901a9ad316367cafb4
tracer-nju <tracer@nju.edu.cn>
2020-06-05 00:46:07 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: b8cb98d4ed304832923355deae58ccb4   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  00:46:07 up 52 min,  1 user,  load average: 0.47, 0.44, 0.39

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -96,7 +96,7 @@ int main(int argc, char* argv[])
     for (void* clusPtr = FirstDataCluster; clusPtr < ImgPtr + fs.st_size; clusPtr += 4096) {
         if (isDirEntryCluster(clusPtr)) {
             printf("|||||||||||%d||||||||\n", (int)(clusPtr - FirstDataCluster)/4096);
-            for (sEntry_t* left = clusPtr; (void*)left < clusPtr + 4064;) {
+            for (sEntry_t* left = clusPtr; (void*)left < clusPtr + 4096;) {
                 sEntry_t* right = left;
                 while (right->DIR_Attr != 0x20) ++right;
                 char name[128];
