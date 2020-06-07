0b281ca0bb6a7d02f637185514ea797dd9acd8a6
tracer-nju <tracer@nju.edu.cn>
2020-06-07 23:57:19 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 9325c2d8f72d4aab802dfed0f386c45d   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  23:57:19 up  7:43,  1 user,  load average: 2.03, 1.85, 1.06

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -174,7 +174,7 @@ int main(int argc, char* argv[])
                                 memcpy(tmpbuf + BytesPerCluster, ptr2, Min(bmpsize, BytesPerCluster));
                                 int i = 0;
                                 for (; i + width * 3 < BytesPerCluster + Min(bmpsize, BytesPerCluster); i++) {
-                                    if (abs(tmpbuf[i] - tmpbuf[i + width * 3]) <= 100) rational_cnt++;
+                                    if (abs(tmpbuf[i] - tmpbuf[i + width * 3]) <= 30) rational_cnt++;
                                 }
                                 if (3 * rational_cnt >= 2 * i) {
                                     fwrite(ptr2, Min(bmpsize, BytesPerCluster), 1, fp);
@@ -190,7 +190,7 @@ int main(int argc, char* argv[])
                                         memcpy(tmpbuf, ptr1, BytesPerCluster);
                                         memcpy(tmpbuf + BytesPerCluster, ptr2, Min(bmpsize, BytesPerCluster));
                                         for (int k = 0; k + width * 3 < BytesPerCluster + Min(bmpsize, BytesPerCluster); k++) {
-                                            if (abs(tmpbuf[k] - tmpbuf[k + width * 3]) <= 100) rational_cnt++;
+                                            if (abs(tmpbuf[k] - tmpbuf[k + width * 3]) <= 30) rational_cnt++;
                                         }
                                         if(rational_cnt > current_rational_cnt){
                                             current_rational_cnt = rational_cnt;
