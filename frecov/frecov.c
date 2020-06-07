139f566fe3ac1e3f839488539270764eaafae5d0
tracer-nju <tracer@nju.edu.cn>
2020-06-08 00:01:13 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 9325c2d8f72d4aab802dfed0f386c45d   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  00:01:13 up  7:47,  1 user,  load average: 0.59, 1.26, 1.01

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -176,7 +176,7 @@ int main(int argc, char* argv[])
                                 for (; i + width * 3 < BytesPerCluster + Min(bmpsize, BytesPerCluster); i++) {
                                     if (abs(tmpbuf[i] - tmpbuf[i + width * 3]) <= 30) rational_cnt++;
                                 }
-                                if (3 * rational_cnt >= 2 * i) {
+                                if (2 * rational_cnt >= 1 * i) {
                                     fwrite(ptr2, Min(bmpsize, BytesPerCluster), 1, fp);
                                     bmpsize -= Min(bmpsize, BytesPerCluster);
                                     ptr1 = ptr2;
