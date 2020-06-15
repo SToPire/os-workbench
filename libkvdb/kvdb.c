e42bd88e92ae7b8fecd6944a6a7698306e64553e
tracer-nju <tracer@nju.edu.cn>
2020-06-15 22:49:00 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 0ea77360ae614ad88dd3e68960744115   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-59-generic       Architecture: x86-64  22:49:00 up  1:12,  1 user,  load average: 0.26, 0.33, 0.27

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
 
 struct kvdb {
-  // your definition here
+    int fd;
 };
+typedef struct kvdb kvdb_t;
 
-struct kvdb *kvdb_open(const char *filename) {
-  return NULL;
+struct kvdb* kvdb_open(const char* filename)
+{
+    kvdb_t* ptr = malloc(sizeof(kvdb_t));
+    ptr->fd = open(filename, O_CREAT | O_RDWR);
+    return ptr;
 }
 
-int kvdb_close(struct kvdb *db) {
-  return -1;
+int kvdb_close(struct kvdb* db)
+{
+    return -1;
 }
 
-int kvdb_put(struct kvdb *db, const char *key, const char *value) {
-  return -1;
+int kvdb_put(struct kvdb* db, const char* key, const char* value)
+{
+    return -1;
 }
 
-char *kvdb_get(struct kvdb *db, const char *key) {
-  return NULL;
+char* kvdb_get(struct kvdb* db, const char* key)
+{
+    return NULL;
 }
