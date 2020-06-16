62f41620f07860fa95e396e84e5735a6c55c87f9
tracer-nju <tracer@nju.edu.cn>
2020-06-16 23:16:58 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 6c5104f83a814543809a5de6b91112e1   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-59-generic       Architecture: x86-64  23:16:58 up  5:22,  1 user,  load average: 1.07, 0.61, 0.38

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -75,7 +75,14 @@ void kvdb_journal(kvdb_t* db){
         char* val = malloc(4096);
         sscanf(buf, " %s %s", key, val);
 
-        find_end(db->fd);
+        lseek(fd, 0, SEEK_END);
+        lseek(fd, -1, SEEK_CUR);
+        char buf;
+        read(fd, &buf, 1);
+        while (buf != '\n') {
+            lseek(fd, -2, SEEK_CUR);
+            read(fd, &buf, 1);
+        }
         write(db->fd, key, strlen(key));
         write(db->fd, " ", 1);
         write(db->fd, val, strlen(val));
