0c6ae8738ae7ba39555190b9bf5bd765e64f1b0a
tracer-nju <tracer@nju.edu.cn>
2020-06-28 22:43:25 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: b1f514fdf1e94c038c6e09eb02c40954   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-59-generic       Architecture: x86-64  22:43:26 up  3:47,  1 user,  load average: 0.95, 0.66, 0.67

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -53,11 +53,10 @@ static void tty_reader(void* arg)
     snprintf(ps, 16, "(%s) $ ", arg);
     while (1) {
         tty->ops->write(tty, 0, ps, strlen(ps));
-        printf("1");
-        int nread = tty->ops->read(tty, 0, cmd, sizeof(cmd) - 1);
-        cmd[nread] = '\0';
-        sprintf(resp, "tty reader task: got %d character(s).\n", strlen(cmd));
-        tty->ops->write(tty, 0, resp, strlen(resp));
+        // int nread = tty->ops->read(tty, 0, cmd, sizeof(cmd) - 1);
+        // cmd[nread] = '\0';
+        // sprintf(resp, "tty reader task: got %d character(s).\n", strlen(cmd));
+        // tty->ops->write(tty, 0, resp, strlen(resp));
     }
 }
 
