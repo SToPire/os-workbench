83b91ef358c9ffd81481816f2ab7ed6a558d2bba
tracer-nju <tracer@nju.edu.cn>
2020-06-28 23:36:01 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 8410f888013e4d38b9ddaeca760df4ee   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-59-generic       Architecture: x86-64  23:36:02 up 9 min,  1 user,  load average: 1.67, 0.92, 0.48

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -75,14 +75,14 @@ static void os_init()
 
     spin_init(&lk, NULL);
 
-    task_t* t1 = pmm->alloc(sizeof(task_t));
-    task_t* t2 = pmm->alloc(sizeof(task_t));
-    task_t* t3 = pmm->alloc(sizeof(task_t));
+    // task_t* t1 = pmm->alloc(sizeof(task_t));
+    // task_t* t2 = pmm->alloc(sizeof(task_t));
+    // task_t* t3 = pmm->alloc(sizeof(task_t));
 
-    kmt->create(t1, "th1", th, "th1");
-    kmt->create(t2, "th2", th, "th2");
-    kmt->create(t3, "th3", th, "th3");
-    kmt->create(pmm->alloc(sizeof(task_t)), "th4", th4, "th4");
+    // kmt->create(t1, "th1", th, "th1");
+    // kmt->create(t2, "th2", th, "th2");
+    // kmt->create(t3, "th3", th, "th3");
+    // kmt->create(pmm->alloc(sizeof(task_t)), "th4", th4, "th4");
 
     // kmt->sem_init(&empty, "empty", 5);  // 缓冲区大小为 5
     // kmt->sem_init(&fill, "fill", 0);
