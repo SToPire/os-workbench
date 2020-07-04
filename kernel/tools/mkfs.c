f126bd4269a459272a60317914a1e10a41631e6d
tracer-nju <tracer@nju.edu.cn>
2020-07-04 15:31:50 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 7b4707e722964c0783c478dcf2bf2e7c   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-62-generic       Architecture: x86-64  15:31:50 up  1:13,  1 user,  load average: 0.58, 0.60, 0.41

--- kernel/tools/mkfs.c
+++ kernel/tools/mkfs.c
@@ -1,20 +1,26 @@
 #include <user.h>
 #include <stdio.h>
+#include<stdlib.h>
 #include <assert.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 #include <sys/mman.h>
 
-#define IMG_SIZE (64 * 1024 * 1024)
-
 int main(int argc, char *argv[]) {
   int fd;
   uint8_t *disk;
 
   // TODO: argument parsing
+  int IMG_SIZE = atoi(argv[1]);
 
-  assert((fd = open(argv[1], O_RDWR)) > 0);
+  char* cwd = getcwd(NULL, 0);
+  char* newwd = malloc(strlen(cwd) - strlen("/tools") + 1);
+  strncpy(newwd, cwd, strlen(cwd) - strlen("/tools"));
+  if (chdir(newwd) != 0) assert(0);
+  printf("%d %s\n", IMG_SIZE, newwd);
+
+  assert((fd = open(argv[2], O_RDWR)) > 0);
   assert((ftruncate(fd, IMG_SIZE)) == 0);
   assert((disk = mmap(NULL, IMG_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)) != (void *)-1);
 
