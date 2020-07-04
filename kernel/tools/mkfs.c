45a6336be1a1b15984c5af287ce89557a7053348
tracer-nju <tracer@nju.edu.cn>
2020-07-04 16:22:17 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 7b4707e722964c0783c478dcf2bf2e7c   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-62-generic       Architecture: x86-64  16:22:18 up  2:04,  1 user,  load average: 0.73, 0.45, 0.42

--- kernel/tools/mkfs.c
+++ kernel/tools/mkfs.c
@@ -7,9 +7,14 @@
 #include <string.h>
 #include <sys/mman.h>
 
-#include "../include/devices.h"
-
 #define FS_OFFSET 1 * 1024 * 1024
+
+typedef struct superblock {
+    uint32_t blk_size;
+    uint32_t fat_head;
+    uint32_t data_head;
+    uint32_t fst_free_data_blk;
+} superblock_t;
 
 int main(int argc, char* argv[])
 {
@@ -27,9 +32,15 @@ int main(int argc, char* argv[])
     assert((ftruncate(fd, IMG_SIZE)) == 0);
     assert((disk = mmap(NULL, IMG_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)) != MAP_FAILED);
 
-    //uint8_t* fs_head = disk + FS_OFFSET;
-    //fs_head[0] = 'A';
+    uint8_t* fs_head = disk + FS_OFFSET;
 
+    superblock_t sb;
+    sb.blk_size = 32;
+    sb.fat_head = 16;
+    sb.data_head = 1024;
+    sb.fst_free_data_blk = 0;
+
+    memcpy(fs_head, (void*)(&sb), sizeof(sb));
     munmap(disk, IMG_SIZE);
     close(fd);
 }
