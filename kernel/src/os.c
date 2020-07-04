7bff5e5f4383baf9da45f5aa2b61225496238bbe
tracer-nju <tracer@nju.edu.cn>
2020-07-04 16:11:00 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 7b4707e722964c0783c478dcf2bf2e7c   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-62-generic       Architecture: x86-64  16:11:00 up  1:52,  1 user,  load average: 0.37, 0.40, 0.44

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -92,7 +92,7 @@ static void os_init()
     // for (int i = 0; i <2; i++)  // 5 个消费者
     //     kmt->create(pmm->alloc(sizeof(task_t)), "consumer", consumer, NULL);
 
-    vfs_init();
+    vfs->init();
 }
 
 static void os_run()
