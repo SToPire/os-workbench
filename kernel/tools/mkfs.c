1e745dda7f377fbbd7a6f2e5a501056387a66028
tracer-nju <tracer@nju.edu.cn>
2020-07-04 16:06:09 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 7b4707e722964c0783c478dcf2bf2e7c   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-62-generic       Architecture: x86-64  16:06:09 up  1:47,  1 user,  load average: 0.44, 0.47, 0.47

--- kernel/tools/mkfs.c
+++ kernel/tools/mkfs.c
@@ -1,31 +1,33 @@
 #include <user.h>
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 #include <sys/mman.h>
 
-int main(int argc, char *argv[]) {
-  int fd;
-  uint8_t *disk;
+#define FS_OFFSET 1 * 1024 * 1024
 
-  // TODO: argument parsing
-  int IMG_SIZE = atoi(argv[1]) * 1024 * 1024;
+int main(int argc, char* argv[])
+{
+    int fd;
+    uint8_t* disk;
 
-  char* cwd = getcwd(NULL, 0);
-  char* newwd = malloc(strlen(cwd) - strlen("/tools") + 1);
-  strncpy(newwd, cwd, strlen(cwd) - strlen("/tools"));
-  if (chdir(newwd) != 0) assert(0);
-  printf("%d %s\n", IMG_SIZE, newwd);
+    int IMG_SIZE = atoi(argv[1]) * 1024 * 1024;
 
-  assert((fd = open(argv[2], O_RDWR)) > 0);
-  assert((ftruncate(fd, IMG_SIZE)) == 0);
-  assert((disk = mmap(NULL, IMG_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)) != (void *)-1);
+    char* cwd = getcwd(NULL, 0);
+    char* newwd = malloc(strlen(cwd) - strlen("/tools") + 1);
+    strncpy(newwd, cwd, strlen(cwd) - strlen("/tools"));
+    assert(chdir(newwd) == 0);
 
-  // TODO: mkfs
+    assert((fd = open(argv[2], O_RDWR)) > 0);
+    assert((ftruncate(fd, IMG_SIZE)) == 0);
+    assert((disk = mmap(NULL, IMG_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)) != MAP_FAILED);
 
-  munmap(disk, IMG_SIZE);
-  close(fd);
+    //uint8_t* fs_head = disk + FS_OFFSET;
+    //fs_head[0] = 'A';
+
+    munmap(disk, IMG_SIZE);
+    close(fd);
 }
