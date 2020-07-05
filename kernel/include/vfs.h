888d86e32ac2253fc22df88d622f57e4051c2028
tracer-nju <tracer@nju.edu.cn>
2020-07-06 00:24:36 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 0af50b18d9c04c5383d5fc81dd5b71d2   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-62-generic       Architecture: x86-64  00:24:36 up  3:37,  1 user,  load average: 0.48, 0.29, 0.27

--- kernel/include/vfs.h
+++ kernel/include/vfs.h
@@ -1,11 +1,33 @@
 #ifndef __VFS_H__
 #define __VFS_H__
 
-typedef struct superblock {
+typedef struct _superblock {
     uint32_t blk_size;
     uint32_t fat_head;
     uint32_t data_head;
     uint32_t fst_free_data_blk;
 } superblock_t;
+
+enum INODE_TYPE {
+    T_INVALID,
+    T_DIR,
+    T_FILE,
+};
+
+typedef struct _inode inode_t;
+struct _inode {
+    int type;
+    char path[128];
+
+    inode_t* parent;
+    inode_t* firstChild;
+    inode_t* nxtBrother;
+};
+
+typedef struct _file{
+    int fd;
+    //char path[128];
+    inode_t* inode;
+} file_t;
 
 #endif
--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -2,15 +2,64 @@
 #include <devices.h>
 #include <vfs.h>
 
+inode_t* inodeSearch(inode_t* cur, const char* path)
+{
+    for (inode_t* ptr = cur->firstChild; ptr != NULL; ptr = ptr->nxtBrother) {
+        if(strncmp(path,ptr->path,strlen(ptr->path) == 0)){
+            if (strlen(path) == strlen(ptr->path))
+                return ptr;
+            else
+                return inodeSearch(ptr, path);  
+        }
+    }
+    return cur;
+}
+
+void inodeInsert(inode_t* parent, inode_t* cur)
+{
+    if (parent->firstChild == NULL)
+        parent->firstChild = cur;
+    else {
+        inode_t* i;
+        for (; i->nxtBrother; i = i->nxtBrother)
+            ;
+        i->nxtBrother = cur;
+    }
+}
+
+void inodeDelete(inode_t* parent, inode_t* child)
+{
+    if (child == parent->firstChild)
+        parent->firstChild = child->nxtBrother;
+    else {
+        inode_t* i;
+        for (; i && i->nxtBrother != child; i = i->nxtBrother)
+            ;
+        if (i)
+            i->nxtBrother = child->nxtBrother;
+    }
+}
+superblock_t sb;
+device_t* sda;
+void addFAT(uint32_t from, uint32_t to)
+{
+    
+}
+
+
 #define FS_OFFSET 1 * 1024 * 1024
+inode_t* root;
 
 void vfs_init()
 {
-    superblock_t sb;
-    device_t* sda = dev->lookup("sda");
+    sda = dev->lookup("sda");
     sda->ops->read(sda, FS_OFFSET, &sb, sizeof(sb));
-
     printf("%u %u %u %u\n", sb.blk_size, sb.data_head, sb.fat_head, sb.fst_free_data_blk);
+
+    root->firstChild = root->nxtBrother = NULL;
+    root->parent = root;
+    sprintf(root->path, "/");
+    root->type = T_DIR;
 }
 
 MODULE_DEF(vfs) = {
