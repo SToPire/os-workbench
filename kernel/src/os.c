18d7d4e09232ec2f6943a59f64dba92c456e8bc2
tracer-nju <tracer@nju.edu.cn>
2020-07-10 02:06:32 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: dc286a25203046ed8259197d628ff702   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-62-generic       Architecture: x86-64  02:06:32 up  6:14,  1 user,  load average: 0.81, 0.76, 0.68

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -65,7 +65,7 @@
 void vfs_test(){
     int a = vfs->open("/a", O_CREAT);
     int b = vfs->open("/bcd", O_CREAT);
-    vfs->write(0, "123456789123456789123456789123456", 33);
+    vfs->write(0, "Make America Great AGAINNNNNNNNNNNNNN!", 38);
     //vfs->write(0, "NULL", 4);
 
     while (1)
