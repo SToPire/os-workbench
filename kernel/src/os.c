a65b2733d3d10dbb75c48e81edea657dd678b3b7
tracer-nju <tracer@nju.edu.cn>
2020-07-12 23:31:44 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 1692887e48d742ea8219e38710e959c2   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-62-generic       Architecture: x86-64  23:31:44 up  6:20,  1 user,  load average: 1.37, 1.02, 0.57

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -72,6 +72,7 @@ void vfs_test()
     vfs->write(b, "Hello world!", 12);
 
     char* s = pmm->alloc(128);
+    memset(s, 0, 128);
     vfs->lseek(a, 0, 0);
     vfs->read(a, s, 10);
     printf("%s\n", s);
