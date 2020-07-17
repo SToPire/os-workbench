b0199cb0c583a88dc4d05b3113cd46d7388fd6e5
tracer-nju <tracer@nju.edu.cn>
2020-07-18 02:27:56 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 2f9d757519184d1a8625d4ab8687bf98   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-62-generic       Architecture: x86-64  02:27:56 up  1:37,  1 user,  load average: 0.68, 0.80, 0.85

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -294,9 +294,7 @@ int vfs_open(const char* pathname, int flags)
             }
             current->fds[free_fd] = fst_ofile_ptr;
             ofiles[fst_ofile_ptr] = newFile;
-            printf("here:%s %d %d\n", pathname, newFile->fd,fst_ofile_ptr);
             ++cnt_ofile;
-            printf("ff:%d\n", newFile->fd);
             return newFile->fd;
         }
     } else {   //do not create file
