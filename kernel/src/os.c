e340b2fe6b402ec23be3ada170d999b110cfcb2e
tracer-nju <tracer@nju.edu.cn>
2020-07-18 02:19:58 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 2f9d757519184d1a8625d4ab8687bf98   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-62-generic       Architecture: x86-64  02:19:58 up  1:29,  1 user,  load average: 1.04, 1.21, 0.91

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -66,7 +66,7 @@ void vfs_test()
 {
     int a = vfs->open("/a", O_CREAT);
     int b = vfs->open("/b", O_CREAT);
-    int c = vfs->open("/c",O_CREAT);
+    int c = vfs->open("/c",0);
     //vfs->close(b);
     int d = vfs->open("/d", O_CREAT);
     printf("%d %d %d %d\n", a, b, c, d);
