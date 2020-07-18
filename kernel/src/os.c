85054bad568841282d040f2d5010419bf754f5e7
tracer-nju <tracer@nju.edu.cn>
2020-07-18 22:02:57 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 74d64f5cc6074ee998c4dab5617b8bc1   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-62-generic       Architecture: x86-64  22:02:57 up  2:40,  1 user,  load average: 1.17, 0.85, 0.53

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -87,7 +87,7 @@ void vfs_test()
     memset(s, 0, 128);
     vfs->read(v4, s, 6);
     assert(s[0] == 0 && strcmp(s + 1, "54321")==0);
-    for (int i = 0; i < 6; i++) printf("%x ", s[i]);
+    for (int i = 0; i < 10; i++) printf("%x ", s[i]);
 
     while (1)
         ;
