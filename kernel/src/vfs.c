dd09eebc87e4c722931e30ccb2701de389bd7dd0
tracer-nju <tracer@nju.edu.cn>
2020-07-20 00:08:06 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: c3be4ae297344407bcc0e1e60a2112d9   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-62-generic       Architecture: x86-64  00:08:06 up  2:29,  1 user,  load average: 1.39, 1.37, 1.30

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -33,6 +33,7 @@ inode_t* inodeSearch(inode_t* cur, const char* path)
     } else {
         strncpy(curName, path + 1, i - 1);
     }
+    printf("ss:%s\n", curName);
     for (inode_t* ptr = cur->firstChild; ptr != NULL; ptr = ptr->nxtBrother) {
         if (strcmp(ptr->name, curName) == 0) {
             if (i == strlen(path))
