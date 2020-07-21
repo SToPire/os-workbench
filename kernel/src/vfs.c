bb2564096d3f249d8af59ea54f5e9b93f94028ee
tracer-nju <tracer@nju.edu.cn>
2020-07-21 23:39:37 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 7bd4835b2d8a4fd6b9de5109acc3e942   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-62-generic       Architecture: x86-64  23:39:37 up  3:11,  1 user,  load average: 1.32, 0.95, 0.69

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -133,7 +133,7 @@ void traverse_dir(inode_t* curRoot, dinode_t* curDinode)
         if (e->dir_entry.name[0] == 0) continue;
         dinode_t dinode;
         sda->ops->read(sda, FS_OFFSET + sb.inode_head + sb.inode_size * e->dir_entry.inode, &dinode, sizeof(dinode_t));
-        printf("%s %u\n", e->dir_entry.name, dinode.stat.id);
+//        printf("%s %u\n", e->dir_entry.name, dinode.stat.id);
 
         inode_t* newInode = pmm->alloc(sizeof(inode_t));
         newInode->dInodeNum = dinode.stat.id;
