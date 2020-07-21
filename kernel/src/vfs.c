a1349a477926a416c8f4d53259c825e1b7f03e3f
tracer-nju <tracer@nju.edu.cn>
2020-07-22 00:19:36 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 7bd4835b2d8a4fd6b9de5109acc3e942   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-62-generic       Architecture: x86-64  00:19:36 up  3:51,  1 user,  load average: 0.65, 0.41, 0.34

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -132,13 +132,13 @@ void traverse_dir(inode_t* curRoot, dinode_t* curDinode)
         readEntry(curBlk, e);
         curBlk = getNextFAT(curBlk);
         if (e->dir_entry.name[0] == 0) continue;
-        dinode_t dinode;
-        sda->ops->read(sda, FS_OFFSET + sb.inode_head + sb.inode_size * e->dir_entry.inode, &dinode, sizeof(dinode_t));
-        printf("%s %u\n", e->dir_entry.name, dinode.stat.id);
+        dinode_t* dinode = pmm->alloc(sizeof(dinode_t));
+        sda->ops->read(sda, FS_OFFSET + sb.inode_head + sb.inode_size * e->dir_entry.inode, dinode, sizeof(dinode_t));
+        printf("%s %u\n", e->dir_entry.name, dinode->stat.id);
 
         inode_t* newInode = pmm->alloc(sizeof(inode_t));
-        newInode->dInodeNum = dinode.stat.id;
-        newInode->firstBlock = dinode.firstBlock;
+        newInode->dInodeNum = dinode->stat.id;
+        newInode->firstBlock = dinode->firstBlock;
         strcpy(newInode->name, e->dir_entry.name);
         inodeInsert(curRoot, newInode);
 
