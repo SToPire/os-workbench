a50df3bf48f0c2e524e971ef59298867bb0d6bff
tracer-nju <tracer@nju.edu.cn>
2020-07-21 21:20:41 +0800

    Static hostname: zyf-ThinkPad-E480          Icon name: computer-laptop            Chassis: laptop         Machine ID: cefd83657d264a1f81ca5530ecd2e923            Boot ID: 7bd4835b2d8a4fd6b9de5109acc3e942   Operating System: Ubuntu 19.10             Kernel: Linux 5.3.0-62-generic       Architecture: x86-64  21:20:41 up 52 min,  1 user,  load average: 0.65, 0.57, 0.43

--- kernel/tools/mkfs.c
+++ kernel/tools/mkfs.c
@@ -162,7 +162,7 @@ void traverse(char* pathname, uint32_t parentino)
             } else {
                 sprintf(fullPath, "%s%s", pathname, dir_entry->d_name);
             }
-            printf("%s\n", fullPath);
+            traverse(fullPath, d.inode);
         }
     }
     memcpy(fs_head + sb.inode_head + sb.inode_size * dirInode.stat.id, (void*)(&dirInode), sizeof(dinode_t));
